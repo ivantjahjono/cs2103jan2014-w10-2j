860fb30d6bf6e6b5f75a8ad17c8f80e057ba6e43
Abdul Hadi <damienhadios@gmail.com>
2014-02-27 11:21:51 +0800

    Update UI to respond to update from the controller and display them.

--- Task Master Kaboom/src/KaboomGUI.java
+++ Task Master Kaboom/src/KaboomGUI.java
@@ -20,7 +20,6 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.table.DefaultTableModel;
-
 import java.awt.SystemColor;
 import java.awt.FlowLayout;
 import java.util.Vector;
@@ -28,10 +27,13 @@ import java.util.Vector;
 
 public class KaboomGUI implements ActionListener {
 
+	private final int MAX_TASK_DISPLAY_COUNT = 10;
+	
 	private JFrame frame;
 	private JTextField txtEnterCommandHere;
 	private JLabel lblFeedback;
-
+	private static JTable tasklistDisplay;
+	
 	/**
 	 * Launch the application.
 	 */	
@@ -66,7 +68,7 @@ public class KaboomGUI implements ActionListener {
 		frame = new JFrame();
 		frame.setBackground(UIManager.getColor("Button.darkShadow"));
 		frame.setResizable(false);
-		frame.setBounds(100, 100, 500, 300);
+		frame.setBounds(100, 100, 600, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -78,7 +80,7 @@ public class KaboomGUI implements ActionListener {
 		
 		txtEnterCommandHere = new JTextField();
 		txtEnterCommandHere.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		txtEnterCommandHere.setBounds(10, 231, 474, 29);
+		txtEnterCommandHere.setBounds(10, 231, 574, 29);
 		txtEnterCommandHere.setText("Enter command here");
 		frame.getContentPane().add(txtEnterCommandHere);
 		txtEnterCommandHere.setColumns(10);
@@ -87,17 +89,17 @@ public class KaboomGUI implements ActionListener {
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setBounds(10, 40, 474, 147);
+		scrollPane.setBounds(10, 40, 574, 147);
 		frame.getContentPane().add(scrollPane);
 		
-		JTable tasklist = new JTable();
-		tasklist.setAutoscrolls(false);
-		tasklist.setUpdateSelectionOnSort(false);
-		tasklist.getTableHeader().setReorderingAllowed(true);
-		tasklist.setFillsViewportHeight(true);
-		tasklist.setShowHorizontalLines(false);
-		tasklist.setShowVerticalLines(false);
-		tasklist.setModel(new DefaultTableModel(
+		tasklistDisplay = new JTable();
+		tasklistDisplay.setAutoscrolls(false);
+		tasklistDisplay.setUpdateSelectionOnSort(false);
+		tasklistDisplay.getTableHeader().setReorderingAllowed(true);
+		tasklistDisplay.setFillsViewportHeight(true);
+		tasklistDisplay.setShowHorizontalLines(false);
+		tasklistDisplay.setShowVerticalLines(false);
+		tasklistDisplay.setModel(new DefaultTableModel(
 			new Object[][] {
 				{new Integer(1), "Meeting", "1:00pm", "2:00pm", "***"},
 				{new Integer(2), "CS 1101 Lecture", "3:00pm", "3:30pm", "**"},
@@ -113,29 +115,39 @@ public class KaboomGUI implements ActionListener {
 			 * 
 			 */
 			private static final long serialVersionUID = 1L;
-
+			Class[] columnTypes = new Class[] {
+				Integer.class, String.class, String.class, String.class, String.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+			boolean[] columnEditables = new boolean[] {
+				false, false, false, false, false
+			};
 			public boolean isCellEditable(int row, int column) {
-				return false;
+				return columnEditables[column];
 			}
 		});
-		tasklist.getColumnModel().getColumn(0).setResizable(false);
-		tasklist.getColumnModel().getColumn(0).setPreferredWidth(24);
-		tasklist.getColumnModel().getColumn(1).setResizable(false);
-		tasklist.getColumnModel().getColumn(1).setPreferredWidth(150);
-		tasklist.getColumnModel().getColumn(2).setResizable(false);
-		tasklist.getColumnModel().getColumn(3).setResizable(false);
-		tasklist.getColumnModel().getColumn(4).setResizable(false);
-		tasklist.getColumnModel().getColumn(4).setPreferredWidth(45);
-		tasklist.setRowSelectionAllowed(false);
-		scrollPane.setViewportView(tasklist);
-		tasklist.setBorder(null);
+		tasklistDisplay.getColumnModel().getColumn(0).setResizable(false);
+		tasklistDisplay.getColumnModel().getColumn(0).setPreferredWidth(21);
+		tasklistDisplay.getColumnModel().getColumn(1).setResizable(false);
+		tasklistDisplay.getColumnModel().getColumn(1).setPreferredWidth(150);
+		tasklistDisplay.getColumnModel().getColumn(2).setResizable(false);
+		tasklistDisplay.getColumnModel().getColumn(2).setPreferredWidth(100);
+		tasklistDisplay.getColumnModel().getColumn(3).setResizable(false);
+		tasklistDisplay.getColumnModel().getColumn(3).setPreferredWidth(100);
+		tasklistDisplay.getColumnModel().getColumn(4).setResizable(false);
+		tasklistDisplay.getColumnModel().getColumn(4).setPreferredWidth(45);
+		tasklistDisplay.setRowSelectionAllowed(false);
+		scrollPane.setViewportView(tasklistDisplay);
+		tasklistDisplay.setBorder(null);
 		
 		JPanel panel = new JPanel();
 		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
 		flowLayout.setAlignment(FlowLayout.LEFT);
 		panel.setAlignmentX(Component.LEFT_ALIGNMENT);
 		panel.setBackground(SystemColor.controlHighlight);
-		panel.setBounds(10, 203, 474, 24);
+		panel.setBounds(10, 203, 574, 24);
 		frame.getContentPane().add(panel);
 		
 		lblFeedback = new JLabel("Feedback");
@@ -155,8 +167,46 @@ public class KaboomGUI implements ActionListener {
 		updateFeedbackTextfield(feedback);
 	}
 	
-	public void updateTasksDisplay (String feedback, Vector<TaskInfo> taskList) {
+	public void updateUiDisplay (String feedback, Vector<TaskInfo> taskList) {
 		updateFeedbackTextfield(feedback);
+		updateTaskDisplay(taskList);
+	}
+	
+	private void updateTaskDisplay (Vector<TaskInfo> taskList) {
+		updateAllTaskDisplayRows(taskList);
+		removeAllRowsAfterIndex(taskList.size());
+	}
+
+	private void updateAllTaskDisplayRows(Vector<TaskInfo> taskList) {
+		for (int i = 0; i < taskList.size() && i < MAX_TASK_DISPLAY_COUNT; i++) {
+			TaskInfo currentTaskInfo = taskList.get(i);
+			
+			TaskInfoDisplay infoToDisplay = new TaskInfoDisplay();
+			infoToDisplay.updateFromThisInfo(currentTaskInfo);
+			infoToDisplay.setTaskId(i+1);
+			updateThisRowData(infoToDisplay);
+		}
+	}
+	
+	private void updateThisRowData (TaskInfoDisplay rowData) {
+		int rowIndex = rowData.getTaskId()-1;
+		
+		tasklistDisplay.setValueAt(rowData.getTaskId(), rowIndex, 0);
+		tasklistDisplay.setValueAt(rowData.getTaskName(), rowIndex, 1);
+		tasklistDisplay.setValueAt(rowData.getStartDate(), rowIndex, 2);
+		tasklistDisplay.setValueAt(rowData.getEndDate(), rowIndex, 3);
+		tasklistDisplay.setValueAt(rowData.getImportanceLevel(), rowIndex, 4);
+	}
+	
+	private void removeAllRowsAfterIndex (int index) {
+		while (tasklistDisplay.getRowCount() > index) {
+			removeThisRowForTaskDisplay(index);
+		}
+	}
+	
+	private void removeThisRowForTaskDisplay (int rowIndex) {
+		DefaultTableModel currentTableModal = (DefaultTableModel)tasklistDisplay.getModel();
+		currentTableModal.removeRow(rowIndex);
 	}
 	
 	private void resetCommandTextfield () {
