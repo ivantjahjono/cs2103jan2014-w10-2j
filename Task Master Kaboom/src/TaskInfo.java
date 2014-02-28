2cbc8038a1fbf145d91505397e4df0b77ff31983
Abdul Hadi <damienhadios@gmail.com>
2014-02-28 11:42:59 +0800

    Updated class diagram to include history class.

---
+++ Task Master Kaboom/src/TaskInfo.java
@@ -0,0 +1,62 @@
+import java.util.Calendar; 
+
+public class TaskInfo {
+	public enum TASK_TYPE {
+		DEADLINE, FLOATING, TIMED;
+	}
+	
+	String taskname;
+	TASK_TYPE taskType;
+	
+	Calendar startDate;		// This includes the time as well
+	Calendar endDate;		// This includes the time as well
+	
+	int importanceLevel;
+	
+	TaskInfo () {
+		taskname = "";
+		startDate = null;
+		endDate = null;
+		importanceLevel = 0;
+	}
+	
+	public void setTaskName (String name) { 
+		taskname = name;
+	}
+	
+	public void setTaskType (TASK_TYPE type) { 
+		taskType = type;
+	}
+	
+	public void setStartDate (Calendar date) { 
+		startDate = date;
+	}
+	
+	public void setEndDate (Calendar date) { 
+		endDate = date;
+	}
+	
+	public void setImportanceLevel (int level) { 
+		importanceLevel = level;
+	}
+	
+	public String getTaskName () { 
+		return taskname;
+	}
+	
+	public TASK_TYPE getTaskType () { 
+		return taskType;
+	}
+	
+	public Calendar getStartDate () { 
+		return startDate;
+	}
+	
+	public Calendar getEndDate () { 
+		return endDate;
+	}
+	
+	public int getImportanceLevel () { 
+		return importanceLevel;
+	}
+}
---
+++ Task Master Kaboom/src/TaskListShop.java
@@ -0,0 +1,50 @@
+import java.util.Vector;
+
+public class TaskListShop {
+
+	private Vector<TaskInfo> taskList;
+	
+	private static TaskListShop taskListInstance = null;
+	
+	public static TaskListShop getInstance () {
+		if (taskListInstance == null) {
+			taskListInstance = new TaskListShop();
+		}
+		
+		return taskListInstance;
+	}
+	
+	private TaskListShop () {
+		taskList = new Vector<TaskInfo>();
+	}
+	
+	public void addTaskToList (TaskInfo newTask) {
+		taskList.add(newTask);
+	}
+	
+	public TaskInfo getTaskByName (String taskName) {
+		for (int i = 0; i < taskList.size(); i++) {
+			if (taskName.equals(taskList.get(i))) {
+				return taskList.get(i);
+			}
+		}
+		return null;
+	}
+	
+	public Vector<TaskInfo> getAllTaskInList () {
+		Vector<TaskInfo> vectorToReturn = new Vector<TaskInfo>(taskList);
+		return vectorToReturn;
+	}
+	
+	public boolean removeTaskByName (String taskName) {
+		TaskInfo currentTaskToRemove = getTaskByName(taskName);
+		
+		if (currentTaskToRemove != null) {
+			taskList.remove(currentTaskToRemove);
+			return true;
+		}
+		
+		return false;
+	}
+	
+}
