ad48fe8c56b1f5d7f9a597226078932414d381b1
Abdul Hadi <damienhadios@gmail.com>
2014-02-28 14:58:41 +0800

    Clean up command class.

--- Task Master Kaboom/src/Command.java
+++ Task Master Kaboom/src/Command.java
@@ -39,51 +39,10 @@ public class Command {
 	}
 	
 	public String execute() {
-		switch(commandType) {
-			case ADD:
-				return add();
-			case DELETE:
-				return delete();
-			case MODIFY: 
-				return modify();
-			case SEARCH:
-				return search();
-			default: 
-				return MESSAGE_COMMAND_INVALID;
-		
-		}
+		return MESSAGE_COMMAND_INVALID;
 	}
 	
 	public String undo () {
 		return MESSAGE_COMMAND_UNDO_FAIL;
 	}
-	
-
-	private String add() {
-		if (TaskListShop.getInstance().addTaskToList(taskInfo)) {
-			return String.format(MESSAGE_COMMAND_ADD_SUCCESS, taskInfo.getTaskName());
-		}
-		return String.format(MESSAGE_COMMAND_ADD_FAIL, taskInfo.getTaskName());
-	}
-	
-	private String delete() {
-		String taskName = taskInfo.getTaskName();
-
-		if (TaskListShop.getInstance().removeTaskByName(taskName)) {
-			return String.format(MESSAGE_COMMAND_DELETE_SUCCESS, taskName);		
-		}
-		
-		return String.format(MESSAGE_COMMAND_DELETE_FAIL, taskName);
-	}
-	
-	//Not done
-	private String modify() {
-		return String.format(MESSAGE_COMMAND_MODIFY_SUCCESS, "My Task");
-	}
-	
-	//Not done
-	private static String search() {
-		return String.format(MESSAGE_COMMAND_SEARCH_SUCCESS, "My Task");
-	}
-	
 }
