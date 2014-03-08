66d1bd1ad3cc0b092d4c1de00e99b9dc93fce721
Abdul Hadi <damienhadios@gmail.com>
2014-03-08 15:58:28 +0800

    Clean up and repackage the java files. Recode to fit the new UI into current working project.

--- Java SpecialFX/src/kaboomUserInterface/MainWindow.java
+++ Java SpecialFX/src/kaboomUserInterface/MainWindow.java
@@ -1,17 +1,22 @@
-package application;
+package kaboomUserInterface;
+
+import main.TaskMasterKaboom;
 
 import java.net.URL; 
 import java.util.ResourceBundle; 
 
-import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
 
-public class MainForm implements javafx.fxml.Initializable {
+public class MainWindow implements javafx.fxml.Initializable {
 	
 	@FXML private TextField commandTextInput;
+	@FXML private Pane feedbackBox;
+	@FXML private Label feedbackText;
 	
 	@Override	 
 	public void initialize(URL location, ResourceBundle resources) {
@@ -21,7 +26,13 @@ public class MainForm implements javafx.fxml.Initializable {
 	private void onTextfieldAction (ActionEvent e) {
 		System.out.println("Hello world!");
 		
+		String command = commandTextInput.getText();
+		String feedback = TaskMasterKaboom.processCommand(command);
+		
 		commandTextInput.setText("");
+		feedbackText.setText(feedback);
+		//resetCommandTextfield();
+		//updateFeedbackTextfield(feedback);
 	}
 	
 	@FXML
---
+++ Java SpecialFX/src/main/TaskInfoDisplay.java
@@ -0,0 +1,80 @@
+package main;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class TaskInfoDisplay {
+	int taskId;
+	String taskname;
+	
+	String startDate;
+	String endDate;
+	
+	String importanceLevel;
+	
+	TaskInfoDisplay () {
+		taskId = 0;
+		taskname = "No taskname available";
+		startDate = "-";
+		endDate = "-";
+		importanceLevel = "";
+	}
+	
+	public void updateFromThisInfo (TaskInfo infoToUpdateFrom) {
+		setTaskName(infoToUpdateFrom.getTaskName());
+		setStartTime(infoToUpdateFrom.getStartDate());
+		setEndTime(infoToUpdateFrom.getEndDate());
+		setImportanceLevel(infoToUpdateFrom.getImportanceLevel());
+	}
+	
+	public void setTaskId (int id) {
+		taskId = id;
+	}
+	
+	public void setTaskName (String name) {
+		taskname = name;
+	}
+	
+	public void setStartTime (Calendar time) {
+		if (time != null) {
+			SimpleDateFormat sdf = new SimpleDateFormat("h:mm a dd/MM/yy");
+			startDate = sdf.format(time.getTime());
+		}
+	}
+	
+	public void setEndTime (Calendar time) {
+		if (time != null) {
+			SimpleDateFormat sdf = new SimpleDateFormat("h:mm a dd/MM/yy");
+			endDate = sdf.format(time.getTime());
+		}
+	}
+	
+	public void setImportanceLevel (int level) {
+		if (level == 0) {
+			importanceLevel = "";
+		} else {
+			for (int i = 0; i < level; i++) {
+				importanceLevel += "*";
+			}
+		}
+	}
+	
+	public int getTaskId () {
+		return taskId;
+	}
+	
+	public String getTaskName () {
+		return taskname;
+	}
+	
+	public String getStartDate () {
+		return startDate;
+	}
+	
+	public String getEndDate () {
+		return endDate;
+	}
+	
+	public String getImportanceLevel () {
+		return importanceLevel;
+	}
+}
