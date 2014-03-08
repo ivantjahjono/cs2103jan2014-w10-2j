a525f5920d5b3a752acfd30b7fa642db74e46c4c
Abdul Hadi <damienhadios@gmail.com>
2014-03-08 14:24:17 +0800

    Testing out using javaFX project.
    **NOTE javaFx needs to be installed for the project to run properly**

---
+++ Java SpecialFX/src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
+	
+	Parent root;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			root = FXMLLoader.load(getClass().getResource("TaskMasterKaboom.fxml"));
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		Scene scene = new Scene(root);
+		//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
