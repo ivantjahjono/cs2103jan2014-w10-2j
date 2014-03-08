736a0083036b73812d6a602ebca75652dc8266fc
Abdul Hadi <damienhadios@gmail.com>
2014-03-08 23:31:02 +0800

    Changed loading of FXML to pass stage info into MainWindow instance.

--- Java SpecialFX/src/kaboomUserInterface/GraphicInterface.java
+++ Java SpecialFX/src/kaboomUserInterface/GraphicInterface.java
@@ -1,5 +1,7 @@
 package kaboomUserInterface;
 
+import java.io.IOException;
+
 import main.TaskMasterKaboom;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -15,15 +17,23 @@ public class GraphicInterface extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) {
+		FXMLLoader loader = null;
 		try {
-			root = FXMLLoader.load(getClass().getResource("TaskMasterKaboom.fxml"));
+			loader = new FXMLLoader(getClass().getResource("TaskMasterKaboom.fxml"));
+			root = (Parent)(loader.load());
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 		
+		MainWindow mainWindow = loader.getController();
+		mainWindow.setStage(primaryStage);
+		
 		Scene scene = new Scene(root);
 		//scene.setFill(Color.rgb(255, 255, 255, 0.5));
 		//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		
+		
+		
 		primaryStage.setScene(scene);
 		primaryStage.setResizable(false);
 		primaryStage.initStyle(StageStyle.TRANSPARENT);
