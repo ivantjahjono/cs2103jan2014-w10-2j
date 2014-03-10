87ac67b02f0c2c3d233e163b32080f3e606f73a7
Ivan Tjahjono <tjahjono.ivan@gmail.com>
2014-03-11 01:40:30 +0800

    added findPriority method and commented currently unused constants

--- Task Master Kaboom/src/CommandSearch.java
+++ Task Master Kaboom/src/CommandSearch.java
@@ -13,21 +13,22 @@ public class CommandSearch extends Command {
 
 
 	public Result execute() {
+		
 		String commandFeedback = "";
-		
-		String taskName = taskInfo.getTaskName();
-		TaskInfo searchTask = taskListShop. getTaskByName(taskName);
-		Vector<TaskInfo> tasksFound = new Vector<TaskInfo>();
-		
-		for (int i = 0; i < allTasks.size(); i++) {
-			TaskInfo singleTask = allTasks.get(i);
-			if (singleTask.getTaskName().contains(taskName)) {
-				tasksFound.add(singleTask);
-			}
-		}
-		commandFeedback = String.format(MESSAGE_COMMAND_SEARCH_SUCCESS, tasksFound.size());
-		
-		return createResult(tasksFound, commandFeedback);
+	 	 
+	 	String taskName = taskInfo.getTaskName();
+	 	Vector<TaskInfo> allTasks = TaskListShop.getInstance().getAllTaskInList();
+	 	Vector<TaskInfo> tasksFound = new Vector<TaskInfo>();
+	 	 
+	 	for (int i = 0; i < allTasks.size(); i++) {
+	 	 TaskInfo singleTask = allTasks.get(i);
+	 	 if (singleTask.getTaskName().contains(taskName)) {
+	 	 tasksFound.add(singleTask);
+	 	 }
+	 	}
+	 	commandFeedback = String.format(MESSAGE_COMMAND_SEARCH_SUCCESS, tasksFound.size());
+	 	 
+	 	return createResult(tasksFound, commandFeedback);
 	}
 	
 }
