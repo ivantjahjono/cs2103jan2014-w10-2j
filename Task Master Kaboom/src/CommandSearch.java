b0547896b6394ae1bd166df46b60a38a1c8565dd
Wei <Wei@localhost>
2014-03-11 18:00:13 +0800

    Added CommandClear. CommandClear removes all taskinfo in the application TaskListShop: added clear task and taskList size method TaskMasterKaboom: added clear command type

---
+++ Task Master Kaboom/src/CommandSearch.java
@@ -0,0 +1,33 @@
+	/*
+	 * Current search is base on task name and result is unique. 
+	 * Search result will be returned in the command's taskInfo variable
+	 * 
+	 */
+import java.util.Vector;
+
+public class CommandSearch extends Command {
+	
+	CommandSearch () {
+		commandType = COMMAND_TYPE.SEARCH;
+	}
+
+
+	public Result execute() {
+		String commandFeedback = "";
+		
+		String taskName = taskInfo.getTaskName();
+		Vector<TaskInfo> allTasks = TaskListShop.getInstance().getAllTaskInList();
+		Vector<TaskInfo> tasksFound = new Vector<TaskInfo>();
+		
+		for (int i = 0; i < allTasks.size(); i++) {
+			TaskInfo singleTask = allTasks.get(i);
+			if (singleTask.getTaskName().contains(taskName)) {
+				tasksFound.add(singleTask);
+			}
+		}
+		commandFeedback = String.format(MESSAGE_COMMAND_SEARCH_SUCCESS, tasksFound.size());
+		
+		return createResult(tasksFound, commandFeedback);
+	}
+	
+}
---
+++ Task Master Kaboom/src/TaskInfo.java
@@ -0,0 +1,97 @@
+import java.util.Calendar; 
+
+enum TASK_TYPE {
+	DEADLINE, FLOATING, TIMED;
+}
+
+public class TaskInfo {
+	
+	String taskname;
+	TASK_TYPE taskType;
+	
+	Calendar startDate;		// This includes the time as well
+	Calendar endDate;		// This includes the time as well
+	
+	int importanceLevel;
+	
+	TaskInfo () {
+		taskname = "";
+		startDate = null;
+		endDate = null;
+		importanceLevel = 0;
+	}
+	
+	public void setTaskName (String name) { 
+		taskname = name;
+	}
+	
+	public void setTaskType (TASK_TYPE type) { 
+		taskType = type;
+	}
+	
+	public void setStartDate (Calendar date) { 
+		startDate = date;
+	}
+	
+	public void setEndDate (Calendar date) { 
+		endDate = date;
+	}
+	
+	public void setImportanceLevel (int level) { 
+		importanceLevel = level;
+	}
+	
+	public String getTaskName () { 
+		return taskname;
+	}
+	
+	public TASK_TYPE getTaskType () { 
+		return taskType;
+	}
+	
+	public Calendar getStartDate () { 
+		return startDate;
+	}
+	
+	public Calendar getEndDate () { 
+		return endDate;
+	}
+	
+	public int getImportanceLevel () { 
+		return importanceLevel;
+	}
+	
+	public boolean isEmpty () {
+	
+		if (taskname == "" && startDate == null && endDate == null && importanceLevel == 0) {
+			return true;
+		}		
+		
+		return false;
+	}
+	
+	public static TASK_TYPE getTaskType(String taskType) {
+		if (taskType.equalsIgnoreCase("DEADLINE")) {
+			return TASK_TYPE.DEADLINE;
+		}
+		else if (taskType.equalsIgnoreCase("FLOATING")) {
+			return TASK_TYPE.FLOATING;
+		}
+		else {
+			return TASK_TYPE.TIMED;
+		}
+	}
+	
+	public static String taskTypeToString(TASK_TYPE taskType) {
+		switch (taskType) {
+		case DEADLINE:
+			return "DEADLINE";
+		case FLOATING:
+			return "FLOATING";
+		case TIMED:
+			return "TIMED";
+		default:
+			return "Unrecognized";  //This should never be reached
+		}
+	}
+}
