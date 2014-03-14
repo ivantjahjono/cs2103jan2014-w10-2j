0584da0f0c8b04ecbf4591da2b422fd95dcee120
Abdul Hadi <damienhadios@gmail.com>
2014-03-14 11:09:23 +0800

    Test updating a counter every 5 secs.

--- Java SpecialFX/src/kaboomUserInterface/GraphicInterface.java
+++ Java SpecialFX/src/kaboomUserInterface/GraphicInterface.java
@@ -1,17 +1,49 @@
 package kaboomUserInterface;
 
 import main.TaskMasterKaboom;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.concurrent.Task;
+import javafx.concurrent.Service;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.util.Duration;
 
 
 public class GraphicInterface extends Application {
 	
 	Parent root;
+	MainWindow mainWindow;
+	
+	public static class MyService extends Service<Void> {
+		
+		MainWindow mainWindow;
+		int counter;
+		
+		MyService (MainWindow window) {
+			mainWindow = window;
+			counter = 0;
+		}
+		
+	    @Override
+	    protected Task<Void> createTask() {
+	      return new Task<Void>() {
+	        @Override
+	        protected Void call() throws Exception {
+	        	System.out.println("Begin task");
+	        	++counter;
+	        	return null;
+	        }
+	      };
+	    }
+	  }
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -23,7 +55,7 @@ public class GraphicInterface extends Application {
 			e.printStackTrace();
 		}
 		
-		MainWindow mainWindow = loader.getController();
+		mainWindow = loader.getController();
 		mainWindow.setStage(primaryStage);
 		
 		Scene scene = new Scene(root);
@@ -36,6 +68,19 @@ public class GraphicInterface extends Application {
 		primaryStage.show();
 		
 		mainWindow.prepareTextfieldFocus();
+		
+		final MyService myService = new MyService(mainWindow);
+
+	    Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(5), new EventHandler<ActionEvent>() {
+	    	@Override
+			public void handle(ActionEvent event) {
+	    		myService.restart();   // automatically on JavaFX thread, so can call restart directly
+	    		mainWindow.updateCounter(myService.counter);
+	    	}
+	    }));
+
+	    timeline.setCycleCount(Animation.INDEFINITE);
+	    timeline.playFrom("end"); // can also play from start but you will have an initial 5 second delay
 	}
 	
 	public void run(String[] args) {
