aafe1d36eb0137622cf2b20db71eafe69fdfaed7
Abdul Hadi <damienhadios@gmail.com>
2014-03-16 14:44:20 +0800

    Include another possible parser. Different method using joining instead of cutting.

---
+++ Java SpecialFX/src/main/KeytypeIndexPair.java
@@ -0,0 +1,36 @@
+package main;
+
+class KeytypeIndexPair {
+	KEYWORD_TYPE type;
+	int indexPosition;
+	
+	public KeytypeIndexPair () {
+		type = KEYWORD_TYPE.INVALID;
+		indexPosition = -1;
+	}
+	
+	public KeytypeIndexPair (KEYWORD_TYPE newType, int position) {
+		setType(newType);
+		setIndexPosition(position);
+	}
+	
+	public void setType (KEYWORD_TYPE newType) {
+		type = newType;
+	}
+	
+	public void setIndexPosition (int position) {
+		indexPosition = position;
+	}
+	
+	public KEYWORD_TYPE getType () {
+		return type;
+	}
+	
+	public int getIndexPosition () {
+		return  indexPosition;
+	}
+	
+	public String toString () {
+		return "[" + type + ", " + indexPosition + "]";
+	}
+}
