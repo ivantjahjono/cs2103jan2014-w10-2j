342ad7f941298ba9f9bf04e1536da85178c6eafe
Abdul Hadi <damienhadios@gmail.com>
2014-03-23 17:59:15 +0800

    Change update service to call the controller update.

--- Java SpecialFX/src/kaboom/ui/UpdateService.java
+++ Java SpecialFX/src/kaboom/ui/UpdateService.java
@@ -2,15 +2,18 @@ package kaboom.ui;
 
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
+import kaboom.logic.TaskMasterKaboom;
 
 class UpdateService extends Service<Void> {
 	
 	MainWindow mainWindow;
 	int counter;
+	TaskMasterKaboom controllerInstance;
 	
 	UpdateService (MainWindow window) {
 		mainWindow = window;
 		counter = 0;
+		controllerInstance = TaskMasterKaboom.getInstance();
 	}
 	
     @Override
@@ -20,6 +23,7 @@ class UpdateService extends Service<Void> {
     		protected Void call() throws Exception {
     			//System.out.println("Begin task");
     			++counter;
+    			controllerInstance.updateTaskList();
     			return null;
     		}
     	};
