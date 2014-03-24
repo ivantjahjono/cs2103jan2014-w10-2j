a1103d9b1a3548c38775fd2c085d1499f43c739d
Ivan Tjahjono <tjahjono.ivan@gmail.com>
2014-03-24 18:56:24 +0800

    change removefirstword and getfirstword to public

---
+++ Java SpecialFX/src/kaboom/logic/TaskMasterKaboom.java
@@ -0,0 +1,197 @@
+package kaboom.logic;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Hashtable;
+import java.util.Random;
+import java.util.Vector;
+import java.util.regex.Pattern;
+
+import kaboom.logic.command.COMMAND_TYPE;
+import kaboom.logic.command.Command;
+import kaboom.logic.command.CommandFactory;
+import kaboom.logic.command.CommandUpdate;
+import kaboom.storage.History;
+import kaboom.storage.Storage;
+import kaboom.storage.TaskListShop;
+import kaboom.ui.GraphicInterface;
+
+
+/*
+** This is main class that will run Task Master KABOOM
+** 
+** 
+**/
+
+public class TaskMasterKaboom {
+	
+	private static final String MESSAGE_WELCOME = "Welcome back, Commander";
+	
+	private static final String FILENAME = "KABOOM_FILE.dat";
+	
+	private static GraphicInterface taskUi;
+	private static DisplayData guiDisplayData;
+	private static Storage fileStorage;
+	private static History historyofCommands = new History();
+
+	
+	static TaskMasterKaboom instance;
+	
+	public static TaskMasterKaboom getInstance () {
+		if (instance == null) {
+			instance = new TaskMasterKaboom();
+		}
+		return instance;
+	}
+	
+	public void initialiseKaboom() {
+		// Setup application
+			// Setup Memory
+			//addTemporaryTaskForTesting();
+		
+			// Setup Storage
+			initialiseStorage();
+		
+			// Setup UI
+			guiDisplayData = DisplayData.getInstance();
+			updateUiWithFirstLoadedMemory();
+			//setupUi();
+			
+			
+			// Setup Logic
+		
+		// Run the UI
+		//activateUi();
+		
+		// Start processing user commands
+	}
+
+	private static void updateUiWithFirstLoadedMemory() {
+		Result introResult = new Result();
+		introResult.setTasksToDisplay(TaskListShop.getInstance().getAllTaskInList());
+		introResult.setFeedback(MESSAGE_WELCOME);
+		updateUi(introResult);
+	}
+	
+	private static boolean initialiseStorage () {
+		fileStorage = new Storage(FILENAME);
+		fileStorage.load();
+		
+		return true;
+	}
+	
+	public void updateTaskList () {
+		Command updateCommand = new CommandUpdate();
+		Result updateResult = updateCommand.execute();
+		
+		updateUi(updateResult);
+	}
+	
+	/*
+	 * Purpose: ProcessCommand will read the userCommand and break down into
+	 * respective information for the task information. Currently, it returns
+	 * the feedback for command that is executed.
+	 * 
+	 * Note: Public access allow execution from test driven development
+	 * to run straight into command.
+	 * 
+	 * Future improvement: Return task class instead.
+	 */
+	public static boolean processCommand(String userInputSentence) {
+		assert userInputSentence != null;
+		
+		Command commandToExecute = null;
+		Result commandResult = null;
+//		COMMAND_TYPE commandType = determineCommandType(userInputSentence);
+//		String commandParametersString = TextParser.removeFirstWord(userInputSentence);
+		
+		String commandKeyword = TextParser.getCommandKeyWord(userInputSentence);
+		commandToExecute = CommandFactory.createCommand(commandKeyword);		
+		Hashtable<KEYWORD_TYPE, String> taskInformationTable = TextParser.extractTaskInformation(userInputSentence);
+		
+		updateCommandInfoBasedOnTaskInformationTable(commandToExecute, taskInformationTable);
+		
+		try {
+			commandResult = commandToExecute.execute();
+		} catch (Exception e) {
+			commandResult = new Result();
+			commandResult.setFeedback("Error executing command! Please inform your administrator!");
+		}
+		
+		updateUi(commandResult);
+		
+		// Add recent command to History list
+		addToCommandHistory(new Command());
+		
+		// Save data to file
+		fileStorage.store();
+		
+		return true;
+	}
+	
+
+	private static void updateUi(Result commandResult) {
+		guiDisplayData.updateDisplayWithResult(commandResult);
+	}
+	
+	private static void addToCommandHistory(Command command) {
+		if (command.getCommandType() != COMMAND_TYPE.INVALID) {
+			historyofCommands.addToRecentCommands(command);
+		}
+	}
+
+	
+	//********************************
+	private static void updateCommandInfoBasedOnTaskInformationTable(Command commandToUpdate, Hashtable<KEYWORD_TYPE, String> taskInformationTable) {
+		
+		TaskInfo newTaskInfo = getNewTaskInfoFromTaskInformationTable(taskInformationTable);
+		TaskInfo taskInfoToModify = getTaskInfoToModifyTaskInformationTable(taskInformationTable);
+		
+		commandToUpdate.setTaskInfo(newTaskInfo);
+		
+		//modify
+		if (!taskInfoToModify.isEmpty()) {
+			commandToUpdate.setTaskInfoToBeModified(taskInfoToModify);
+		}
+
+	}
+
+	private static TaskInfo getNewTaskInfoFromTaskInformationTable(Hashtable<KEYWORD_TYPE, String> taskInformationTable) {
+		TaskInfo taskInfo = new TaskInfo();
+		
+		taskInfo.setTaskName(taskInformationTable.get(KEYWORD_TYPE.TASKNAME));
+		
+		//if (taskInformationTable.contains(KEYWORD_TYPE.START_TIME)) {
+			String startDate = taskInformationTable.get(KEYWORD_TYPE.START_DATE);
+			String startTime = taskInformationTable.get(KEYWORD_TYPE.START_TIME);
+			Calendar startDateAndTime = DateAndTimeFormat.getInstance().formatStringToCalendar(startDate, startTime);
+			taskInfo.setStartDate(startDateAndTime);
+		//}
+		
+		//if (taskInformationTable.contains(KEYWORD_TYPE.END_TIME)) {
+			String endDate = taskInformationTable.get(KEYWORD_TYPE.END_DATE);
+			String endTime = taskInformationTable.get(KEYWORD_TYPE.END_TIME);
+			Calendar endDateAndTime = DateAndTimeFormat.getInstance().formatStringToCalendar(endDate, endTime);
+			taskInfo.setEndDate(endDateAndTime);
+		//}
+				
+		taskInfo.setTaskType(TASK_TYPE.TIMED);	// HARDCODED TO DEFAULT
+		
+		if (taskInformationTable.containsKey(KEYWORD_TYPE.PRIORITY)) {
+			String priority = taskInformationTable.get(KEYWORD_TYPE.PRIORITY);
+			taskInfo.setImportanceLevel(Integer.parseInt(priority));
+		}
+		
+		return taskInfo;
+	}
+
+
+	
+	private static TaskInfo getTaskInfoToModifyTaskInformationTable(Hashtable<KEYWORD_TYPE, String> taskInformationTable) {
+		TaskInfo taskInfo = new TaskInfo();
+		taskInfo.setTaskName(taskInformationTable.get(KEYWORD_TYPE.MODIFIED_TASKNAME));
+		return taskInfo;
+	}
+	
+	
+}
