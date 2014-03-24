6ec6f3d6cfb4ec9c70f96cb72c38e36fa01efd04
Tong Chun Kit <tongchunkit@gmail.com>
2014-03-24 21:26:11 +0800

    CommandSearch now uses the new controller structure Added searching by end date

---
+++ Java SpecialFX/src/kaboom/logic/command/CommandAdd.java
@@ -0,0 +1,44 @@
+package kaboom.logic.command;
+
+import kaboom.logic.Result;
+import kaboom.storage.TaskListShop;
+
+
+
+public class CommandAdd extends Command {
+
+	public CommandAdd () {
+		commandType = COMMAND_TYPE.ADD;
+	}
+
+	public Result execute() {
+		assert taskInfo != null;
+		assert TaskListShop.getInstance() != null;
+		
+		String commandFeedback = "";
+		
+		try {
+			if (taskListShop.addTaskToList(taskInfo)) {
+				commandFeedback = String.format(MESSAGE_COMMAND_ADD_SUCCESS, taskInfo.getTaskName());
+			} else {
+				commandFeedback = String.format(MESSAGE_COMMAND_ADD_FAIL, taskInfo.getTaskName());
+			}
+		} catch (Exception e) {
+			commandFeedback = "Added a file with no Task Name";
+		}
+		
+		return createResult(taskListShop.getAllTaskInList(), commandFeedback);
+	}
+	
+	public String undo () {
+		String taskName = taskInfo.getTaskName();
+		
+		boolean isRemoveSuccess = taskListShop.removeTaskByName(taskName);
+		
+		if (isRemoveSuccess) {
+			return MESSAGE_COMMAND_UNDO_SUCCESS;
+		} else {
+			return MESSAGE_COMMAND_UNDO_FAIL;
+		}
+	}
+}
---
+++ Java SpecialFX/src/kaboom/logic/command/CommandSearch.java
@@ -0,0 +1,56 @@
+package kaboom.logic.command;
+
+import java.util.Vector;
+
+import kaboom.logic.Result;
+import kaboom.logic.TaskInfo;
+import kaboom.logic.KEYWORD_TYPE;
+import kaboom.logic.TASK_TYPE;
+import kaboom.storage.TaskListShop;
+
+public class CommandSearch extends Command {
+	
+	public CommandSearch () {
+		commandType = COMMAND_TYPE.SEARCH;
+		keywordList = new Vector<KEYWORD_TYPE>();
+		keywordList.add(KEYWORD_TYPE.TASKNAME);
+		keywordList.add(KEYWORD_TYPE.END_DATE);
+		keywordList.add(KEYWORD_TYPE.END_TIME);  //Does this work?
+	}
+
+	public Result execute() {
+		
+		assert taskInfo != null;
+		assert TaskListShop.getInstance() != null;
+		String commandFeedback;
+		Vector<TaskInfo> tasksFound = new Vector<TaskInfo>();
+		Vector<TaskInfo> allTasks = TaskListShop.getInstance().getAllTaskInList();
+		
+		String taskName = taskInfo.getTaskName();
+		if (taskName != null) {
+			//If taskName is not null, search by task name			
+			for (int i = 0; i < allTasks.size(); i++) {
+				TaskInfo singleTask = allTasks.get(i);
+				if (singleTask.getTaskName().contains(taskName)) {
+					tasksFound.add(singleTask);
+				}
+			}
+		}
+		else {
+			//search by end date
+			for (int i = 0; i < allTasks.size(); i++) {
+				TaskInfo singleTask = allTasks.get(i);
+				if (!singleTask.getTaskType().equals(TASK_TYPE.FLOATING) 
+						&& singleTask.getEndDate().before(taskInfo.getEndDate())) {
+					tasksFound.add(singleTask);
+				}
+			}
+		}
+		//Search by end time? 
+		
+		commandFeedback = String.format(MESSAGE_COMMAND_SEARCH_SUCCESS, tasksFound.size());
+
+	 	return createResult(tasksFound, commandFeedback);
+	}
+	
+}
