9c3e3a73be5b79da6b8257c631b737e06e56ec8d
Ivan Tjahjono <tjahjono.ivan@gmail.com>
2014-03-25 17:58:53 +0800

    can put extra spaces after keyword "at" or "on"

---
+++ Java SpecialFX/src/KaboomTest/TextParserTest.java
@@ -0,0 +1,168 @@
+package KaboomTest;
+
+import static org.junit.Assert.*;
+
+import java.util.Hashtable;
+import java.util.Queue;
+import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import kaboom.logic.KEYWORD_TYPE;
+import kaboom.logic.TextParser;
+
+import org.junit.Test;
+
+public class TextParserTest {
+	
+	Hashtable<KEYWORD_TYPE, String> keywordTable = new Hashtable<KEYWORD_TYPE, String>();
+
+//	@Test
+//	// Test cutting of words based on keywords
+//	public void testLevel1_KeywordsCheck() {
+//		
+//		assertEquals(-1, TextParser.getNextKeywordIndex("hello world"));
+//		assertEquals(-1, TextParser.getNextKeywordIndex("hello worldon"));
+//		assertEquals(-1, TextParser.getNextKeywordIndex("hello byworld"));
+//		
+//		assertEquals(-1, TextParser.getNextKeywordIndex("hello world by"));
+//		assertEquals("hello world".length(), TextParser.getNextKeywordIndex("hello world by 0700"));
+//		
+//		assertEquals(-1, TextParser.getNextKeywordIndex("by 0700"));
+//		
+//		Vector<Integer> listOfkeywordPosition = TextParser.getListOfKeywordPositions("hello world at 0700 on 14/05/12 by 0200 on 14/05/12");
+//		System.out.println(listOfkeywordPosition);
+//		
+//		listOfkeywordPosition = TextParser.getListOfKeywordPositions("hello world at 0700 on 14/05/12 by 0200 on 14/05/12 ***");
+//		System.out.println(listOfkeywordPosition);
+//	}
+
+	
+	@Test
+	public void testPriorityExtraction () {
+		String command = "";
+		Hashtable<KEYWORD_TYPE, String> tempHashTable = new Hashtable<KEYWORD_TYPE, String>();
+		
+		command = "Test string ***";
+		System.out.println(TextParser.extractPriority(command, tempHashTable));
+		
+		command = "Test string ***   ";
+		System.out.println(TextParser.extractPriority(command, tempHashTable));
+		
+		command = "Test string *";
+		System.out.println(TextParser.extractPriority(command, tempHashTable));
+		
+		command = " **      ";
+		System.out.println(TextParser.extractPriority(command, tempHashTable));
+		
+		command = "asdasd**      ";
+		System.out.println(TextParser.extractPriority(command, tempHashTable));
+		
+		command = "**asdasd      ";
+		System.out.println(TextParser.extractPriority(command, tempHashTable));
+		
+		command = " *";
+		System.out.println(TextParser.extractPriority(command, tempHashTable));
+	}
+	
+//	@Test
+//	public void testTimeExtraction () {
+//		String command = "";
+//		
+//		command = "by 1700";
+//		System.out.println(TextParser.extractTime(command, null));
+//		
+//		command = "by 600";
+//		System.out.println(TextParser.extractTime(command, null));
+//		
+//		command = "by 1800 ";
+//		System.out.println(TextParser.extractTime(command, null));
+//		
+//		command = "by  160";
+//		System.out.println(TextParser.extractTime(command, null));
+//		
+//		command = "by 17:00";
+//		System.out.println(TextParser.extractTime(command, null));
+//		
+//		command = "by 6:00";
+//		System.out.println(TextParser.extractTime(command, null));
+//		
+//		command = "by 180:0 ";
+//		System.out.println(TextParser.extractTime(command, null));
+//		
+//		command = "by 1:6000";
+//		System.out.println(TextParser.extractTime(command, null));
+//		
+//		command = "by :160";
+//		System.out.println(TextParser.extractTime(command, null));
+//		
+//		command = "by :160pm";
+//		System.out.println(TextParser.extractTime(command, null));
+//		
+//		command = "by 6:00ampm";
+//		System.out.println(TextParser.extractTime(command, null));
+//		
+//		command = "by    6:00pmpm ";
+//		System.out.println(TextParser.extractTime(command, null));
+//		
+//		command = "on 12/06/06";
+//		System.out.println(TextParser.extractTime(command, null));
+//		
+//		command = "on 12.06.06";
+//		System.out.println(TextParser.extractTime(command, null));
+//		
+//		command = "on 120606";
+//		System.out.println(TextParser.extractTime(command, null));
+//		
+//		command = "on  120606 ";
+//		System.out.println(TextParser.extractTime(command, null));
+//		
+//		command = "on  1206065  ";
+//		System.out.println(TextParser.extractTime(command, null));
+//		
+//		command = "on  120.06.06 ";
+//		System.out.println(TextParser.extractTime(command, null));
+//		
+//		command = "on  1.06.06 ";
+//		System.out.println(TextParser.extractTime(command, null));
+//		
+//		command = "on  1.6.06 ";
+//		System.out.println(TextParser.extractTime(command, null));
+//		
+//		command = "on  1.6.1906 ";
+//		System.out.println(TextParser.extractTime(command, null));
+//	}
+	
+//	@Test
+//	public void testParser() {
+//		//standard format
+//		keywordTable = new Hashtable<KEYWORD_TYPE, String>();
+//		assertEquals("hello world", TextParser.parser("hello world at 0700 on 14/05/12 by 0200 on 14/05/12 ***",keywordTable));
+//		keywordTable = new Hashtable<KEYWORD_TYPE, String>();
+//		assertEquals("hello world", TextParser.parser("hello world",keywordTable));
+//		keywordTable = new Hashtable<KEYWORD_TYPE, String>();
+//		assertEquals("hello world", TextParser.parser("hello world at 0700 on 14/05/12",keywordTable));
+//		keywordTable = new Hashtable<KEYWORD_TYPE, String>();
+//		assertEquals("hello world", TextParser.parser("hello world by 0200 on 14/05/12",keywordTable));
+//		keywordTable = new Hashtable<KEYWORD_TYPE, String>();
+//		assertEquals("hello world", TextParser.parser("hello world at 0700 on 14/05/12 ***",keywordTable));
+//		keywordTable = new Hashtable<KEYWORD_TYPE, String>();
+//		assertEquals("hello world", TextParser.parser("hello world by 0200 on 14/05/12 ***",keywordTable));
+//		keywordTable = new Hashtable<KEYWORD_TYPE, String>();
+//		assertEquals("hello world", TextParser.parser("hello world ***",keywordTable));
+//		keywordTable = new Hashtable<KEYWORD_TYPE, String>();
+//		assertEquals("hello world", TextParser.parser("hello world by 0200 on 14/05/12 at 0700 on 14/05/12  ***",keywordTable));
+//	}
+	
+	
+//	@Test
+//	public void testExtractModifyName() {
+//		Hashtable<KEYWORD_TYPE, String> keywordTable = new Hashtable<KEYWORD_TYPE, String>();
+//		assertEquals("hello world ", TextParser.extractModifiedTaskName("hello world > some name",keywordTable));
+//		assertEquals("hello world ", TextParser.extractModifiedTaskName("hello world > aabbcc ",keywordTable));
+//		assertEquals("", TextParser.extractModifiedTaskName("> hello world",keywordTable));
+//		assertEquals("hello world", TextParser.extractModifiedTaskName("hello world",keywordTable));
+//		//error here
+//		assertEquals("> hello world ", TextParser.extractModifiedTaskName("> hello world > asd",keywordTable));
+//	}
+}
---
+++ Java SpecialFX/src/kaboom/ui/MainWindow.java
@@ -0,0 +1,595 @@
+package kaboom.ui;
+
+import java.io.IOException;
+import java.net.URL; 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.ResourceBundle; 
+import java.util.Vector;
+
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.RectangleBuilder;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import kaboom.logic.DisplayData;
+import kaboom.logic.TaskInfoDisplay;
+import kaboom.logic.TaskMasterKaboom;
+
+public class MainWindow implements javafx.fxml.Initializable, Observer {
+	//TODO clean up class parameters
+	
+	private final int MAX_TABS = 5;
+	
+	// User interface elements
+			private Stage 		windowStage;
+	@FXML	private AnchorPane 	mainPane;
+	
+	// Table ui elements and its ui columns
+	@FXML 	private TableView<TaskInfoDisplay> 				taskDisplayTable;
+	@FXML	private TableColumn<TaskInfoDisplay, Integer> 	columnTaskId;
+	@FXML 	private TableColumn<TaskInfoDisplay, String> 	columnTaskName;
+	@FXML 	private TableColumn<TaskInfoDisplay, String> 	columnStartTime;
+	@FXML 	private TableColumn<TaskInfoDisplay, String> 	columnEndTime;
+	@FXML 	private TableColumn<TaskInfoDisplay, String> 	columnPriority;
+	
+	// Data for the task table
+			private ObservableList<TaskInfoDisplay> data;
+	
+	// Top window toolbar buttons
+	@FXML 	private ImageView 	exitButton;
+	@FXML 	private Label 		minimiseLabel;
+	@FXML 	private Label 		counter;
+	
+	// Task header to show the current type of tasks displayed
+	@FXML 	private Label header_all;
+	@FXML 	private Label header_running;
+	@FXML 	private Label header_deadline;
+	@FXML 	private Label header_timed;
+	@FXML 	private Label header_search;
+	
+	// List and tracks of previously activated headers
+	private Vector<Label>	labelList;
+	private int 			currentLabelIndex;
+	private int 			previousLabelIndex;
+	
+	// Main command input and user feedback text
+	@FXML private TextField 	commandTextInput;
+	@FXML private Pane 			feedbackBox;
+	@FXML private Label 		feedbackText;
+	
+	// Container to keep the pages tabs 
+	@FXML private HBox 					pageTabContainer;
+		  private ArrayList<Rectangle> 	pagesTab;
+	
+	// Tracks previous commands
+	private String prevCommand;
+	private String currentCommand;
+	
+	// Used in tracking window dragging
+	private double initialX;
+	private double initialY;
+	
+	// Class references
+	private TaskMasterKaboom 	applicationController;
+	private DisplayData 		uiData;
+	
+	// Logging unit and file handler for output
+	private final static Logger loggerUnit = Logger.getLogger(MainWindow.class.getName());
+	private static FileHandler fh;
+	
+	public MainWindow () {
+		prevCommand = "";
+		currentCommand = "";
+		
+		data = FXCollections.observableArrayList();
+		pagesTab = new ArrayList<Rectangle>();
+		
+		uiData = DisplayData.getInstance();
+		
+		labelList = new Vector<Label>();
+		
+		applicationController = TaskMasterKaboom.getInstance();
+	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) throws NullPointerException {
+		createAndStartLogging();
+		
+		try {
+			columnTaskId.setCellValueFactory(new PropertyValueFactory<TaskInfoDisplay, Integer>("taskId"));
+			columnTaskName.setCellValueFactory(new PropertyValueFactory<TaskInfoDisplay, String>("taskName"));
+			columnStartTime.setCellValueFactory(new PropertyValueFactory<TaskInfoDisplay, String>("startDate"));
+			columnEndTime.setCellValueFactory(new PropertyValueFactory<TaskInfoDisplay, String>("endDate"));
+			columnPriority.setCellValueFactory(new PropertyValueFactory<TaskInfoDisplay, String>("importanceLevel"));
+			
+			taskDisplayTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+		} catch (NullPointerException e) {
+			return;
+		}
+		
+		// Disable column reordering
+		//disableTableColumnReordering();
+		
+		mainPane.getStyleClass().add("root");
+		
+		labelList.add(header_all);
+		labelList.add(header_running);
+		labelList.add(header_deadline);
+		labelList.add(header_timed);
+		labelList.add(header_search);
+		currentLabelIndex = 0;
+		previousLabelIndex = 0;
+		
+		setHeaderLabelToSelected(labelList.get(currentLabelIndex));
+		setTablelistToRespondToExpiry();
+		
+		DisplayData.getInstance().addObserver(this);
+		updateDisplay();
+	}
+
+	private void createAndStartLogging() {
+		try {
+			fh = new FileHandler("KaboomUI.log", false);
+		} catch (SecurityException e) {
+			// TODO find ways to handle exceptions
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Logger logger = Logger.getLogger("");
+		fh.setFormatter(new SimpleFormatter());
+		logger.addHandler(fh);
+		logger.setLevel(Level.CONFIG);
+	}
+
+	private void setTablelistToRespondToExpiry() {
+		taskDisplayTable.setRowFactory(new Callback<TableView<TaskInfoDisplay>, TableRow<TaskInfoDisplay>>() {
+	        @Override
+	        public TableRow<TaskInfoDisplay> call(TableView<TaskInfoDisplay> tableView) {
+	            final TableRow<TaskInfoDisplay> row = new TableRow<TaskInfoDisplay>() {
+	                @Override
+	                protected void updateItem(TaskInfoDisplay person, boolean empty){
+	                    super.updateItem(person, empty);
+	                    
+	                    getStyleClass().removeAll(Collections.singleton("isNotExpired"));
+	                    getStyleClass().removeAll(Collections.singleton("isExpired"));
+	                    getStyleClass().removeAll(Collections.singleton("isEmpty"));
+	                    
+	                    if (person == null) {
+	                    	 getStyleClass().add("isEmpty");
+	                    	 return;
+	                    }
+	                    
+	                    if (person.isExpired()) {
+	                    //if (highlightRows.contains(getIndex())) {
+	                        if (! getStyleClass().contains("isExpired")) {
+	                            getStyleClass().add("isExpired");
+	                        }
+	                    } else {   
+	                        getStyleClass().add("isNotExpired");
+	                    }
+	                }
+	            };
+	            return row;
+	        }
+	    });
+	}
+
+	private void disableTableColumnReordering() {
+		taskDisplayTable.getColumns().addListener(new ListChangeListener() {
+	        @Override
+	        public void onChanged(Change change) {
+	          change.next();
+	          if(change.wasReplaced()) {
+	        	  taskDisplayTable.getColumns().clear();
+	        	  taskDisplayTable.getColumns().addAll(columnTaskId, columnTaskName, columnStartTime, columnEndTime, columnPriority);
+	          }
+	        }
+	    });
+	}
+	
+	public void setStage (Stage currentStage) {
+		windowStage = currentStage;
+	}
+	
+	public void prepareTextfieldFocus () {
+		commandTextInput.requestFocus();
+	}
+	
+	@FXML
+	private void onTextfieldAction (ActionEvent e) {
+		String command = commandTextInput.getText();
+		
+		loggerUnit.log(Level.INFO, command);
+		
+		if (command.equals("exit")) {
+			Platform.exit();
+			return;
+		}
+		
+		// Check if need to switch header
+		int switchIndexResult = getSwitchIndexFromCommand(command);
+		if (switchIndexResult != -1) {
+			switchToNewHeader(switchIndexResult);
+		} if (isPageToggle(command)) {
+			activatePageToggle(command);
+		} else {
+			applicationController.processCommand(command);
+		}
+		
+		prevCommand = command;
+		currentCommand = "";
+		
+		commandTextInput.setText("");
+		
+		//updateDisplay();
+	}
+
+	private void switchToNewHeader(int switchIndexResult) {
+		previousLabelIndex = currentLabelIndex;
+		currentLabelIndex = switchIndexResult;
+		switchMainHeaderHighlight(previousLabelIndex, currentLabelIndex);
+	}
+
+	private void updateDisplay() {
+		updateTaskTable();
+		updateFeedbackMessage();
+		updatePagesTab();
+	}
+
+	private void activatePageToggle(String command) {
+		switch (command) {
+			case "next page":
+				uiData.goToNextPage();
+				break;
+				
+			case "prev page":
+				uiData.goToPreviousPage();
+				break;
+				
+			default:
+				break;
+		}
+	}
+
+	private boolean isPageToggle(String command) {
+		switch (command) {
+			case "next page":
+			case "prev page":
+				return true;
+				
+			default:
+				return false;
+		}
+	}
+
+	private void updateTaskTable() {
+		data.clear();
+		
+		Vector<TaskInfoDisplay> taskList = uiData.getTaskDisplay();
+		
+		for (int i = 0; i < taskList.size(); i++) {
+			data.add(taskList.get(i));
+		}
+		taskDisplayTable.setItems(data);
+	}
+	
+	private void updateFeedbackMessage() {
+		String feedback = uiData.getFeedbackMessage();
+		feedbackText.setText(feedback);
+	}
+	
+	private void recallPreviousCommand () {
+		if (!prevCommand.equals(commandTextInput.getText())) {
+			currentCommand = commandTextInput.getText();
+			commandTextInput.setText(prevCommand);
+			commandTextInput.end();
+		}
+	}
+	
+	private void recallStoredTypedCommand () {
+		if (!currentCommand.equals(commandTextInput.getText())) {
+			commandTextInput.setText(currentCommand);
+			commandTextInput.end();
+		}
+	}
+
+	@FXML
+	private void onTextfieldKeyPressed (KeyEvent keyEvent) {
+		//System.out.println("Key pressed: " + keyEvent.getText());
+		
+		switch(keyEvent.getCode()) {
+			case UP:
+				loggerUnit.log(Level.FINE, "Recalling previous command.");
+				recallPreviousCommand();
+				break;
+				
+			case DOWN:
+				loggerUnit.log(Level.FINE, "Recalling next command.");
+				recallStoredTypedCommand();
+				break;
+				
+			case ESCAPE:
+				loggerUnit.log(Level.FINE, "ESC pressed for minimise.");
+				windowStage.setIconified(true);
+				break;
+				
+			case F1:
+				switchToNewHeader(0);
+				break;
+				
+			case F2:
+				switchToNewHeader(1);
+				break;
+				
+			case F3:
+				switchToNewHeader(2);
+				break;
+				
+			case F4:
+				switchToNewHeader(3);
+				break;
+				
+			case F5:
+				switchToNewHeader(4);
+				break;	
+				
+			default:
+				break;
+		}
+	}
+	
+	@FXML
+	private void onExitButtonPressed (MouseEvent mouseEvent) {
+		loggerUnit.log(Level.FINE, "Close button pressed.");
+		Platform.exit();
+	}
+	
+	@FXML
+	private void onWindowMouseDrag (MouseEvent mouseEvent) {
+		//System.out.printf("[%f, %f]\n", mouseEvent.getSceneX(), mouseEvent.getSceneY());
+		
+		windowStage.setX(mouseEvent.getScreenX() - initialX);
+		windowStage.setY(mouseEvent.getScreenY() - initialY);
+	}
+	
+	@FXML
+	private void onWindowMousePressed (MouseEvent mouseEvent) {
+		initialX = mouseEvent.getSceneX();
+		initialY = mouseEvent.getSceneY();
+	}
+	
+	@FXML
+	private void onWindowKeyPressed (KeyEvent keyEvent) {
+		//System.out.println("Key pressed: " + keyEvent.getText());
+		
+		if (!keyEvent.isControlDown()) {
+			return;
+		}
+		
+		previousLabelIndex = currentLabelIndex;
+		switch(keyEvent.getCode()) {
+			case LEFT:
+				currentLabelIndex--;
+				
+				if (currentLabelIndex < 0) {
+					currentLabelIndex = labelList.size()-1;
+				}
+				break;
+				
+			case RIGHT:
+				currentLabelIndex++;
+				currentLabelIndex %= labelList.size();
+				break;
+				
+			default:
+				break;
+		}
+		
+		setHeaderLabelToNormal(labelList.get(previousLabelIndex));
+		setHeaderLabelToSelected(labelList.get(currentLabelIndex));
+	}
+	
+	private int getSwitchIndexFromCommand(String command) {
+		int selected = -1;
+		
+		switch (command) {
+			case "view all":
+				selected = 0;
+				break;
+				
+			case "view running":
+				selected = 1;
+				break;
+				
+			case "view deadline":
+				selected = 2;
+				break;
+				
+			case "view timed":
+				selected = 3;
+				break;
+				
+			case "view search":
+				selected = 4;
+				break;
+				
+			default:
+				break;
+		}
+		
+		return selected;
+	}
+	
+	@FXML
+	private void onHeaderMouseClicked (MouseEvent mouseEvent) {
+		Node nodePressed = (Node)mouseEvent.getSource();
+		
+		loggerUnit.log(Level.FINE, nodePressed.getId()+" header clicked.");
+		
+		previousLabelIndex = currentLabelIndex;
+		switch (nodePressed.getId()) {
+			case "header_all":
+				currentLabelIndex = 0;
+				break;
+				
+			case "header_running":
+				currentLabelIndex = 1;
+				break;
+				
+			case "header_deadline":
+				currentLabelIndex = 2;
+				break;
+				
+			case "header_timed":
+				currentLabelIndex = 3;
+				break;
+				
+			case "header_search":
+				currentLabelIndex = 4;
+				break;
+				
+			default:
+				return;
+		}
+		
+		setHeaderLabelToNormal(labelList.get(previousLabelIndex));
+		setHeaderLabelToSelected(labelList.get(currentLabelIndex));
+	}
+
+	private void switchMainHeaderHighlight(int prevIndex, int currIndex) {
+		setHeaderLabelToNormal(labelList.get(prevIndex));
+		setHeaderLabelToSelected(labelList.get(currIndex));
+	}
+	
+	private void setHeaderLabelToNormal (Label labelToChange) {
+		labelToChange.getStyleClass().removeAll(Collections.singleton("header-label-selected"));
+		labelToChange.getStyleClass().add("header-label-normal");
+	}
+	
+	private void setHeaderLabelToSelected (Label labelToChange) {
+		labelToChange.getStyleClass().removeAll(Collections.singleton("header-label-normal"));
+		labelToChange.getStyleClass().add("header-label-selected");
+	}
+	
+	@FXML
+	private void onMinimiseMousePressed () {
+		loggerUnit.log(Level.FINE, "Minimise button pressed.");
+		windowStage.setIconified(true);
+	}
+	
+	public void updateCounter(int number) {
+		counter.setText("Test Counter: "+number);
+	}
+	
+	@FXML
+	private void onPagesArrowMouseClicked (MouseEvent mouseEvent) {
+		Node nodePressed = (Node)mouseEvent.getSource();
+		
+		loggerUnit.log(Level.FINE, nodePressed.getId()+" button pressed.");
+		switch (nodePressed.getId()) {
+			case "nextArrow":
+				uiData.goToNextPage();
+				break;
+				
+			case "prevArrow":
+				uiData.goToPreviousPage();
+				break;
+		}
+		
+		updateDisplay();
+	}
+
+	private void updatePagesTab() {
+		int maxPages = uiData.getMaxTaskDisplayPages();
+		int maxTabs = maxPages;
+		
+		if (maxTabs > MAX_TABS) {
+			maxTabs = MAX_TABS;
+		}
+		
+		resizePageTabToMaxPages(maxTabs);
+		updatePageTabStyles();
+		refreshPageTabContainerWithNewPageTabs();
+	}
+	
+	private Rectangle createPageTabRectangle() {
+		RectangleBuilder pageTabBuilder = RectangleBuilder.create()
+			    .x(100).y(100)
+			    .width(25).height(5)
+			    .styleClass("pagesOn");
+		
+		Rectangle rect1 = pageTabBuilder.build();
+		return rect1;
+	}
+
+	private void updatePageTabStyles() {
+		int currentTabPage = uiData.getCurrentPage()%MAX_TABS;
+		
+		for (int i = 0; i < pagesTab.size(); i++) {
+			
+			Rectangle currentTab = pagesTab.get(i);
+			currentTab.getStyleClass().removeAll(Collections.singleton("pagesOn"));
+			currentTab.getStyleClass().removeAll(Collections.singleton("pagesOff"));
+			
+			if (i == currentTabPage) {
+				pagesTab.get(i).getStyleClass().add("pagesOn");
+			} else {
+				pagesTab.get(i).getStyleClass().add("pagesOff");
+			}
+		}
+	}
+
+	private void resizePageTabToMaxPages(int maxTab) {
+		if (pagesTab.size() < maxTab) {
+			int tabsToCreate = maxTab - pagesTab.size();
+			for (int i = 0; i < tabsToCreate; i++) {
+				pagesTab.add(createPageTabRectangle());
+			}
+		} else if (pagesTab.size() > maxTab ) {
+			int tabsToDelete = pagesTab.size() - maxTab;
+			for (int i = 0; i < tabsToDelete; i++) {
+				pagesTab.remove(pagesTab.size()-1);
+			}
+		}
+	}
+	
+	private void refreshPageTabContainerWithNewPageTabs() {
+		pageTabContainer.getChildren().clear();
+		pageTabContainer.getChildren().addAll(pagesTab);
+	}
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		updateDisplay();
+	}
+}
