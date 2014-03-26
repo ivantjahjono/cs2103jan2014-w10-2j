5d76f051177b8cad8360f52c59577be1d9076fa9
Tong Chun Kit <tongchunkit@gmail.com>
2014-03-26 22:43:50 +0800

    refreshTasks() in TaskListShop can set tasks as not expired or expired This depends on the end time of the task Floating tasks are not affected

--- Java SpecialFX/src/kaboom/storage/TaskListShop.java
+++ Java SpecialFX/src/kaboom/storage/TaskListShop.java
@@ -11,10 +11,10 @@ public class TaskListShop {
 
 	private static TaskListShop taskListInstance = null;
 	private static final Logger logger = Logger.getLogger("TaskListShopLogger");
-	
+
 	private Vector<TaskInfo> taskList;
-	
-	
+
+
 	public static TaskListShop getInstance () {
 		if (taskListInstance == null) {
 			taskListInstance = new TaskListShop();
@@ -36,7 +36,7 @@ public class TaskListShop {
 			return false;
 		}
 	}
-	
+
 	public TaskInfo getTaskByName (String taskName) {
 		for (int i = 0; i < taskList.size(); i++) {
 			//System.out.println(taskList.get(i).getTaskName());
@@ -65,10 +65,10 @@ public class TaskListShop {
 		Vector<TaskInfo> vectorToReturn = new Vector<TaskInfo>(taskList);
 		return vectorToReturn;
 	}
-	
+
 	public Vector<TaskInfo> getFloatingTasks() {
 		Vector<TaskInfo> returnVector = new Vector<TaskInfo>();
-		
+
 		for (int i = 0; i < taskList.size(); i++) {
 			TaskInfo singleTask = taskList.get(i);
 			if (singleTask.getTaskType() == TASK_TYPE.FLOATING) {
@@ -77,10 +77,10 @@ public class TaskListShop {
 		}
 		return returnVector;
 	}
-	
+
 	public Vector<TaskInfo> getDeadlineTasks() {
 		Vector<TaskInfo> returnVector = new Vector<TaskInfo>();
-		
+
 		for (int i = 0; i < taskList.size(); i++) {
 			TaskInfo singleTask = taskList.get(i);
 			if (singleTask.getTaskType() == TASK_TYPE.DEADLINE) {
@@ -89,10 +89,10 @@ public class TaskListShop {
 		}
 		return returnVector;
 	}
-	
+
 	public Vector<TaskInfo> getTimedTasks() {
 		Vector<TaskInfo> returnVector = new Vector<TaskInfo>();
-		
+
 		for (int i = 0; i < taskList.size(); i++) {
 			TaskInfo singleTask = taskList.get(i);
 			if (singleTask.getTaskType() == TASK_TYPE.TIMED) {
@@ -101,10 +101,10 @@ public class TaskListShop {
 		}
 		return returnVector;
 	}
-	
+
 	public Vector<TaskInfo> getExpiredTasks() {
 		Vector<TaskInfo> returnVector = new Vector<TaskInfo>();
-		
+
 		for (int i = 0; i < taskList.size(); i++) {
 			TaskInfo singleTask = taskList.get(i);
 			boolean isExpired = singleTask.getExpiryFlag();
@@ -114,10 +114,10 @@ public class TaskListShop {
 		}
 		return returnVector;
 	}
-	
+
 	public Vector<TaskInfo> getNonExpiredTasks() {
 		Vector<TaskInfo> returnVector = new Vector<TaskInfo>();
-		
+
 		for (int i = 0; i < taskList.size(); i++) {
 			TaskInfo singleTask = taskList.get(i);
 			boolean isExpired = singleTask.getExpiryFlag();
@@ -139,27 +139,35 @@ public class TaskListShop {
 
 		return false;
 	}
-	
+
 	//This function refreshes all the tasks in the vector to check
 	//whether it has expired and set to true if it has expired.
+	//Sets to false if the task has not expired
+	//Floating tasks have a default of not expired
 	public void refreshTasks() {
 		for (int i = 0; i < taskList.size(); i++) {
 			TaskInfo singleTask = taskList.get(i);
 			Calendar now = Calendar.getInstance();
 			if (now.after(singleTask.getEndDate())) {
-				if (!singleTask.getTaskType().equals(TASK_TYPE.FLOATING))
+				if (!singleTask.getTaskType().equals(TASK_TYPE.FLOATING)) {
 					singleTask.setExpiryFlag(true);  //Floating tasks cannot expire
+				}
+			}
+			else {
+				if (!singleTask.getTaskType().equals(TASK_TYPE.FLOATING)) {
+					singleTask.setExpiryFlag(false);
+				}
 			}
 		}
 	}
-	
+
 	public Vector<TaskInfo> clearAllTasks () {
 		taskList = new Vector<TaskInfo>();
 		Vector<TaskInfo> vectorToReturn = new Vector<TaskInfo>(taskList);
 		logger.info("All tasks cleared");
 		return vectorToReturn;
 	}
-	
+
 	public int shopSize () {
 		return taskList.size();
 	}
