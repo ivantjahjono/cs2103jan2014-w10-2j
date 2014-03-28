d440402cd3d15ac39039cf2c9075ecccf03b4d2c
Tong Chun Kit <tongchunkit@gmail.com>
2014-03-28 19:53:31 +0800

    Added a isDone flag for TaskInfo and methods to get and set it

---
+++ Java SpecialFX/src/kaboom/logic/command/CommandSearch.java
@@ -0,0 +1,72 @@
+package kaboom.logic.command;
+
+import java.util.Calendar;
+import java.util.Vector;
+
+import kaboom.logic.Result;
+import kaboom.logic.TaskInfo;
+import kaboom.logic.KEYWORD_TYPE;
+import kaboom.logic.TASK_TYPE;
+import kaboom.storage.TaskListShop;
+
+public class CommandSearch extends Command {
+
+	public CommandSearch () {
+		commandType = COMMAND_TYPE.SEARCH;
+		initialiseKeywordList();
+	}
+
+	public Result execute() {
+
+		assert taskInfo != null;
+		assert TaskListShop.getInstance() != null;
+		String commandFeedback;
+		Vector<TaskInfo> tasksFound = new Vector<TaskInfo>();
+		Vector<TaskInfo> allTasks = TaskListShop.getInstance().getNonExpiredTasks();
+
+		String taskName = taskInfo.getTaskName();
+		if (!taskName.equals("")) {
+			//If taskName is not empty, search by task name			
+			for (int i = 0; i < allTasks.size(); i++) {
+				TaskInfo singleTask = allTasks.get(i);
+				if (!singleTask.getExpiryFlag()) {
+					//Tasks must not be expired
+					if (singleTask.getTaskName().contains(taskName)) {
+						tasksFound.add(singleTask);
+					}
+				}
+			}
+		}
+		else {
+			//search by date
+			for (int i = 0; i < allTasks.size(); i++) {
+				TaskInfo singleTask = allTasks.get(i);
+				if (!singleTask.getExpiryFlag()) {
+					//Tasks must not be expired
+					if (!singleTask.getTaskType().equals(TASK_TYPE.FLOATING)) {
+						Calendar targetDate = taskInfo.getEndDate();
+						if (singleTask.getEndDate().before(targetDate)) {
+							//For deadline tasks
+							tasksFound.add(singleTask);
+						}
+						if (singleTask.getStartDate().after(targetDate) && singleTask.getEndDate().before(targetDate)) {
+							//For timed tasks
+							tasksFound.add(singleTask);
+						}
+					}
+				}
+			}
+		}
+
+		commandFeedback = String.format(MESSAGE_COMMAND_SEARCH_SUCCESS, tasksFound.size());
+
+		return createResult(tasksFound, commandFeedback);
+	}
+
+	private void initialiseKeywordList() {
+		keywordList.clear();
+		keywordList.add(KEYWORD_TYPE.END_DATE);
+		keywordList.add(KEYWORD_TYPE.END_TIME);
+		keywordList.add(KEYWORD_TYPE.TASKNAME);
+	}
+}
