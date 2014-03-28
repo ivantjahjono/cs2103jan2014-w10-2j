5a011446924156cab789e856b55bafa42fed027d
Tong Chun Kit <tongchunkit@gmail.com>
2014-03-28 22:04:31 +0800

    Searches duplicates the tasks in History as well as the task IDs

--- Java SpecialFX/src/kaboom/logic/command/CommandSearch.java
+++ Java SpecialFX/src/kaboom/logic/command/CommandSearch.java
@@ -7,6 +7,7 @@ import kaboom.logic.Result;
 import kaboom.logic.TaskInfo;
 import kaboom.logic.KEYWORD_TYPE;
 import kaboom.logic.TASK_TYPE;
+import kaboom.storage.History;
 import kaboom.storage.TaskListShop;
 
 public class CommandSearch extends Command {
@@ -20,13 +21,16 @@ public class CommandSearch extends Command {
 
 		assert taskInfo != null;
 		assert TaskListShop.getInstance() != null;
-		
+
 		//Set the end time to 2359 for searching
 		Calendar endDate = taskInfo.getEndDate();
 		endDate.set(Calendar.HOUR_OF_DAY, 23);
 		endDate.set(Calendar.MINUTE, 59);
 		taskInfo.setEndDate(endDate);
-		
+
+		History history = History.getInstance();
+		history.taskID.clear();
+
 		String commandFeedback;
 		Vector<TaskInfo> tasksFound = new Vector<TaskInfo>();
 		Vector<TaskInfo> allTasks = TaskListShop.getInstance().getNonExpiredTasks();
@@ -39,6 +43,7 @@ public class CommandSearch extends Command {
 				if (!singleTask.getExpiryFlag()) {
 					//Tasks must not be expired
 					if (singleTask.getTaskName().contains(taskName)) {
+						history.taskID.add(TaskListShop.getInstance().getAllTaskInList().indexOf(singleTask));
 						tasksFound.add(singleTask);
 					}
 				}
@@ -55,11 +60,13 @@ public class CommandSearch extends Command {
 						if (singleTask.getEndDate().before(targetDate)) {
 							//For deadline tasks
 							tasksFound.add(singleTask);
+							history.taskID.add(TaskListShop.getInstance().getAllTaskInList().indexOf(singleTask));
 						}
 						if (singleTask.getStartDate().before(targetDate) && singleTask.getEndDate().after(targetDate)) {
 							//For timed tasks
 							if (!tasksFound.contains(singleTask)) {
 								tasksFound.add(singleTask);
+								history.taskID.add(TaskListShop.getInstance().getAllTaskInList().indexOf(singleTask));
 							}
 						}
 					}
@@ -67,6 +74,7 @@ public class CommandSearch extends Command {
 			}
 		}
 
+		history.tasksToView = new Vector<TaskInfo>(tasksFound);
 		commandFeedback = String.format(MESSAGE_COMMAND_SEARCH_SUCCESS, tasksFound.size());
 
 		return createResult(tasksFound, commandFeedback);
@@ -78,7 +86,7 @@ public class CommandSearch extends Command {
 		keywordList.add(KEYWORD_TYPE.END_TIME);
 		keywordList.add(KEYWORD_TYPE.TASKNAME);
 	}
-	
+
 	public boolean parseInfo(String info) {
 		return true;
 	}
