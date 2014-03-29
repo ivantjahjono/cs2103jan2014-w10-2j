e9ab791a0fd433a5e0ca687b25b94bb479bb5fcb
Abdul Hadi <damienhadios@gmail.com>
2014-03-29 17:53:33 +0800

    Remove unused test cases.

--- Java SpecialFX/src/KaboomTest/TextParserTest.java
+++ Java SpecialFX/src/KaboomTest/TextParserTest.java
@@ -17,27 +17,6 @@ import org.junit.Test;
 public class TextParserTest {
 	
 	Hashtable<KEYWORD_TYPE, String> keywordTable = new Hashtable<KEYWORD_TYPE, String>();
-
-//	@Test
-//	// Test cutting of words based on keywords
-//	public void testLevel1_KeywordsCheck() {
-//		
-//		assertEquals(-1, TextParser.getNextKeywordIndex("hello world"));
-//		assertEquals(-1, TextParser.getNextKeywordIndex("hello worldon"));
-//		assertEquals(-1, TextParser.getNextKeywordIndex("hello byworld"));
-//		
-//		assertEquals(-1, TextParser.getNextKeywordIndex("hello world by"));
-//		assertEquals("hello world".length(), TextParser.getNextKeywordIndex("hello world by 0700"));
-//		
-//		assertEquals(-1, TextParser.getNextKeywordIndex("by 0700"));
-//		
-//		Vector<Integer> listOfkeywordPosition = TextParser.getListOfKeywordPositions("hello world at 0700 on 14/05/12 by 0200 on 14/05/12");
-//		System.out.println(listOfkeywordPosition);
-//		
-//		listOfkeywordPosition = TextParser.getListOfKeywordPositions("hello world at 0700 on 14/05/12 by 0200 on 14/05/12 ***");
-//		System.out.println(listOfkeywordPosition);
-//	}
-
 	
 	@Test
 	public void testPriorityExtraction () {
