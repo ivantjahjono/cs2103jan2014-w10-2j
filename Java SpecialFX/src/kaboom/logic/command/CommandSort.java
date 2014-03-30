f0265f4873d2c94de97928d1e929f3881e3f65f7
Tong Chun Kit <tongchunkit@gmail.com>
2014-03-30 13:29:36 +0800

    Add removeTaskByID() function in TaskListShop

---
+++ Java SpecialFX/src/kaboom/logic/command/CommandSort.java
@@ -0,0 +1,46 @@
+package kaboom.logic.command;
+
+import kaboom.logic.KEYWORD_TYPE;
+import kaboom.logic.Result;
+
+public class CommandSort extends Command {
+
+	private static final String MESSAGE_COMMAND_SORT_SUCCESS = "Sort done. %d item(s) sorted.";
+
+	public CommandSort() {
+		commandType = COMMAND_TYPE.SORT;
+		initializeKeywordList();
+	}
+
+	public Result execute() {
+		assert taskInfo != null;
+		assert taskListShop != null;
+
+		if (!taskInfo.getTaskName().equals("")) {
+			taskListShop.sort(KEYWORD_TYPE.TASKNAME);
+		}
+		else if (taskInfo.getStartDate() != null) {
+			taskListShop.sort(KEYWORD_TYPE.START_DATE);
+
+		}
+		else if (taskInfo.getEndDate() != null) {
+			taskListShop.sort(KEYWORD_TYPE.END_DATE);
+		}
+		else {
+			taskListShop.sort(KEYWORD_TYPE.PRIORITY);
+		}
+
+		String commandFeedback = String.format(MESSAGE_COMMAND_SORT_SUCCESS, taskListShop.shopSize());
+		return createResult(taskListShop.getAllCurrentTasks(), commandFeedback);
+	}
+
+	public void initializeKeywordList() {
+		keywordList.clear();
+		keywordList.add(KEYWORD_TYPE.TASKNAME);
+		keywordList.add(KEYWORD_TYPE.START_DATE);
+		keywordList.add(KEYWORD_TYPE.START_TIME);
+		keywordList.add(KEYWORD_TYPE.END_DATE);
+		keywordList.add(KEYWORD_TYPE.END_TIME);
+		keywordList.add(KEYWORD_TYPE.PRIORITY);
+	}
+}
