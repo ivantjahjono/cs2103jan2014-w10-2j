c1c68bc899ab8c90ad9236bd1f9757730a193a53
Tong Chun Kit <tongchunkit@gmail.com>
2014-03-30 22:44:52 +0800

    CommandUndone now works with ID and bugs fixed

--- Java SpecialFX/src/kaboom/logic/command/CommandUndone.java
+++ Java SpecialFX/src/kaboom/logic/command/CommandUndone.java
@@ -10,56 +10,64 @@ public class CommandUndone extends Command{
 	private static final String MESSAGE_COMMAND_UNDONE_SUCCESS = "Set %1$s to incomplete";
 	private static final String MESSAGE_COMMAND_UNDONE_AlEADY_INCOMPLETE = "%1$s was incomplete";
 	private static final String MESSAGE_COMMAND_UNDONE_FAIL = "%1$s does not exist";
-	
+
 	TaskInfo taskToBeModified;
-	
+
 	public CommandUndone() {
 		commandType = COMMAND_TYPE.UNDONE;
 		initialiseKeywordList();
 	}
-	
+
 	public Result execute() {
 		assert taskListShop != null;
-		
+
 		String feedback = "";
 		String taskName = taskInfo.getTaskName();
-		taskToBeModified = taskListShop.getTaskByName(taskName);
-		
+		if (isNumeric(taskName)) {
+			taskToBeModified = taskListShop.getArchivedTaskByID(Integer.parseInt(taskName)-1);
+		} else {
+			taskToBeModified = taskListShop.getArchivedTaskByName(taskName);
+		}
+
 		if(taskToBeModified == null) {
 			//can throw exception (task does not exist)
 			feedback = String.format(MESSAGE_COMMAND_UNDONE_FAIL, taskName);
 			return createResult(taskListShop.getAllCurrentTasks(), feedback);
 		}
-		
+
 		if(!taskToBeModified.getDone()) {
 			//can throw exception (command incomplete)
 			feedback = String.format(MESSAGE_COMMAND_UNDONE_AlEADY_INCOMPLETE, taskName);
 			return createResult(taskListShop.getAllCurrentTasks(), feedback);
 		}
-		
+
 		taskInfo = new TaskInfo(taskToBeModified);
 		taskInfo.setDone(false);
-		taskListShop.updateTask(taskInfo, taskToBeModified);
+		taskListShop.updateArchivedTask(taskInfo, taskToBeModified);
 		feedback = String.format(MESSAGE_COMMAND_UNDONE_SUCCESS, taskName);
 		return createResult(taskListShop.getAllCurrentTasks(), feedback);
 	}
-	
+
 	public boolean undo() {
 		taskListShop.updateTask(taskToBeModified, taskInfo);
 		return true;
 	}
-	
+
 	private void initialiseKeywordList() {
 		keywordList.clear();
 		keywordList.add(KEYWORD_TYPE.TASKNAME);
 	}
-	
+
 	protected void storeTaskInfo(Hashtable<KEYWORD_TYPE, String> infoHashes) {
 		taskInfo = new TaskInfo();
 		saveTaskName(infoHashes,taskInfo);
 	}
-	
+
 	public boolean parseInfo(String info) {
 		return true;
+	}
+	
+	private boolean isNumeric(String taskName) {
+		return taskName.matches("\\d{1,4}");
 	}
 }
