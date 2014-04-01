4bb91bd6e2c525b13f3099c452d2ee1a6582bb06
Wei <Wei@localhost>
2014-04-01 18:46:04 +0800

    Fixed delete

---
+++ Java SpecialFX/src/kaboom/logic/command/CommandDone.java
@@ -0,0 +1,98 @@
+package kaboom.logic.command;
+
+import java.util.Hashtable;
+import java.util.Vector;
+
+import kaboom.logic.FormatIdentify;
+import kaboom.logic.KEYWORD_TYPE;
+import kaboom.logic.Result;
+import kaboom.logic.TaskInfo;
+import kaboom.storage.History;
+
+public class CommandDone extends Command{
+	private static final String MESSAGE_COMMAND_DONE_SUCCESS = "Set %1$s to complete";
+	private static final String MESSAGE_COMMAND_DONE_AlEADY_COMPLETED = "%1$s was completed";
+	private static final String MESSAGE_COMMAND_DONE_FAIL = "%1$s does not exist";
+
+	TaskInfo taskToBeModified;
+	Hashtable<KEYWORD_TYPE,String> taskInfoTable;
+	String commandFeedback;
+
+	public CommandDone() {
+		commandType = COMMAND_TYPE.DONE;
+		initialiseKeywordList();
+		taskInfoTable = null; 
+	}
+
+	public Result execute() {
+		assert taskListShop != null;
+
+		String feedback = "";
+		String taskName = taskInfo.getTaskName();
+
+		int taskCount = taskListShop.numOfTasksWithSimilarNames(taskName);
+
+		if (taskCount > 1) {
+			commandFeedback = "OH YEA! CLASH.. BOO000000000M!";
+
+			Command search = new CommandSearch();
+			search.storeTaskInfo(taskInfoTable);
+			return search.execute();
+		}
+		else if (isNumeric(taskName)) {
+			int index = History.getInstance().taskID.get(Integer.parseInt(taskName)-1);
+			taskToBeModified = taskListShop.getTaskByID(index);
+			taskListShop.setDoneByID(index);
+		} else {
+			taskToBeModified = taskListShop.getTaskByName(taskName);
+			taskListShop.setDoneByName(taskName);
+		}
+
+		if (taskToBeModified == null) {
+			//can throw exception (task does not exist)
+			feedback = String.format(MESSAGE_COMMAND_DONE_FAIL, taskName);
+			return createResult(taskListShop.getAllCurrentTasks(), feedback);
+		}
+
+		if (taskToBeModified.getDone()) {
+			//can throw exception (command incomplete)
+			feedback = String.format(MESSAGE_COMMAND_DONE_AlEADY_COMPLETED, taskName);
+			return createResult(taskListShop.getAllCurrentTasks(), feedback);
+		}
+
+		taskInfo.setRecent(true);
+		feedback = String.format(MESSAGE_COMMAND_DONE_SUCCESS, taskName);
+		return createResult(taskListShop.getAllCurrentTasks(), feedback);
+	}
+
+	public boolean undo() {
+		taskListShop.setLastToUndone();
+		return true;
+	}
+
+	private void initialiseKeywordList() {
+		keywordList.clear();
+		keywordList.add(KEYWORD_TYPE.TASKNAME);
+	}
+
+	protected void storeTaskInfo(Hashtable<KEYWORD_TYPE, String> infoHashes) {
+		taskInfo = new TaskInfo();
+		taskInfoTable = infoHashes;
+		saveTaskName(infoHashes, taskInfo);
+	}
+
+	public boolean parseInfo(String info, Vector<FormatIdentify> indexList) {
+		Hashtable<KEYWORD_TYPE, String> taskInformationTable = updateFormatList(info, indexList);
+		updateFormatListBasedOnHashtable(indexList, taskInformationTable);
+
+		if (taskInformationTable.containsKey(KEYWORD_TYPE.INVALID)) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private boolean isNumeric(String taskName) {
+		return taskName.matches("\\d{1,4}");
+	}
+}
