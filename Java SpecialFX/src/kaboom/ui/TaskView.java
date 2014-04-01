8a675b7ff981218d93ce764e860e91550b0ac962
Abdul Hadi <damienhadios@gmail.com>
2014-04-01 22:16:33 +0800

    Branch out for V0.3

---
+++ Java SpecialFX/src/kaboom/ui/TaskView.java
@@ -0,0 +1,168 @@
+package kaboom.ui;
+
+import java.util.Vector;
+
+import kaboom.logic.DisplayData;
+import kaboom.logic.TaskInfo;
+import kaboom.storage.TaskListShop;
+
+public class TaskView {
+
+	private static TaskView instance = null;
+
+	private Vector<TaskInfo> currentView;  //Current view
+	private Vector<TaskInfo> searchView;  //Vector for searches
+	private Vector<Integer> currentViewID;  //Vector for position of viewing tasks in actual vector
+	private TaskListShop taskListShop;
+	private DisplayData displayData;
+
+	public TaskView() {
+		taskListShop = TaskListShop.getInstance();
+		currentView = taskListShop.getAllCurrentTasks();
+		currentViewID = taskListShop.getCorrespondingID(currentView);
+		searchView = new Vector<TaskInfo>();
+		displayData = DisplayData.getInstance();
+	}
+
+	public static TaskView getInstance () {
+		if (instance == null) {
+			instance = new TaskView();
+		}
+		return instance;
+	}
+
+	public Vector<TaskInfo> setAndGetView(DISPLAY_STATE displayState) {
+		switch (displayState) {
+		case ALL:
+			setCurrentView(taskListShop.getAllCurrentTasks());
+			break;
+
+		case RUNNING:
+			setCurrentView(taskListShop.getFloatingTasks());
+			break;
+
+		case DEADLINE:
+			setCurrentView(taskListShop.getDeadlineTasks());
+			break;
+
+		case TIMED:
+			setCurrentView(taskListShop.getTimedTasks());
+			break;
+
+		case SEARCH:
+			setCurrentView(searchView);
+			break;
+
+		case ARCHIVE:
+			setCurrentView(taskListShop.getAllArchivedTasks());
+			break;
+
+		default:
+			setCurrentView(taskListShop.getAllCurrentTasks());
+			System.out.println("Encountered an invalid view!");
+			break;
+		}
+		return currentView;
+	}
+
+	public Vector<TaskInfo> getView(DISPLAY_STATE displayState) {
+		switch (displayState) {
+		case ALL:
+			return taskListShop.getAllCurrentTasks();
+
+		case RUNNING:
+			return taskListShop.getFloatingTasks();
+
+		case DEADLINE:
+			return taskListShop.getDeadlineTasks();
+
+		case TIMED:
+			return taskListShop.getTimedTasks();
+
+		case SEARCH:
+			return searchView;
+
+		case ARCHIVE:
+			return taskListShop.getAllArchivedTasks();
+
+		default:
+			System.out.println("Encountered an invalid view!");
+			return taskListShop.getAllCurrentTasks();
+		}
+	}
+
+	public Vector<Integer> getCurrentViewID() {
+		return currentViewID;
+	}
+
+	public void setSearchView(Vector<TaskInfo> taskList) {
+		searchView = taskList;
+		setCurrentView(taskList);
+	}
+
+	public Vector<TaskInfo> getSearchView() {
+		return searchView;
+	}
+
+	public int getIndexFromView(int index) {
+		return currentViewID.get(index);
+	}
+
+	public void deleteInView(TaskInfo task) {
+		for (int i = 0; i < currentView.size(); i++) {
+			if (currentView.get(i).equals(task)) {
+				currentView.remove(i);
+			}
+		}
+
+		for (int i = 0; i < searchView.size(); i++) {
+			if (searchView.get(i).equals(task)) {
+				searchView.remove(i);
+			}
+		}
+	}
+
+	public void doneInView(TaskInfo task) {
+		for (int i = 0; i < currentView.size(); i++) {
+			if (currentView.get(i).equals(task) && 
+					displayData.getCurrentDisplayState() != DISPLAY_STATE.ARCHIVE) {
+				currentView.remove(i);
+			}
+		}
+
+		for (int i = 0; i < searchView.size(); i++) {
+			if (searchView.get(i).equals(task)) {
+				searchView.remove(i);
+			}
+		}
+	}
+
+	public void undoneInView(TaskInfo task) {
+		for (int i = 0; i < currentView.size(); i++) {
+			if (currentView.get(i).equals(task) && 
+					displayData.getCurrentDisplayState() == DISPLAY_STATE.ARCHIVE) {
+				currentView.remove(i);
+			}
+		}
+
+		//no support for undone tasks in search yet
+		/*
+		for (int i = 0; i < searchView.size(); i++) {
+			if (searchView.get(i).equals(task)) {
+				searchView.remove(i);
+			}
+		}*/
+	}
+
+	public void addToView(TaskInfo task) {
+		if (displayData.getCurrentDisplayState() == DISPLAY_STATE.SEARCH) {
+			searchView.add(task);
+		}
+	}
+
+	public void setCurrentView(Vector<TaskInfo> taskList) {
+		currentView = taskList;
+		currentViewID = taskListShop.getCorrespondingID(taskList);
+	}
+
+}
