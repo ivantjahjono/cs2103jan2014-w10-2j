bc20ea9df875a11d7a816f14de08d8660bf82dca
Tong Chun Kit <tongchunkit@gmail.com>
2014-04-05 21:59:49 +0800

    Changed the comparators for start and end dates for sorting

--- Java SpecialFX/src/kaboom/logic/command/ComparatorEndDate.java
+++ Java SpecialFX/src/kaboom/logic/command/ComparatorEndDate.java
@@ -7,11 +7,12 @@ import kaboom.logic.TaskInfo;
 public class ComparatorEndDate implements Comparator<TaskInfo> {
 
 	public int compare(TaskInfo task1, TaskInfo task2) {
-		if (task1.getEndDate() != null) {
+		if (task1.getEndDate() != null && task2.getEndDate() != null) {
 			return task1.getEndDate().compareTo(task2.getEndDate());
-		}
-		else {
-			return -1;
+		} else if (task1.getStartDate() != null && task2.getStartDate() != null) {
+			return task1.getStartDate().compareTo(task2.getStartDate());
+		} else {
+			return 0;  //Both are equal
 		}
 	}
 }
--- Java SpecialFX/src/kaboom/logic/command/ComparatorStartDate.java
+++ Java SpecialFX/src/kaboom/logic/command/ComparatorStartDate.java
@@ -5,12 +5,14 @@ import java.util.Comparator;
 import kaboom.logic.TaskInfo;
 
 public class ComparatorStartDate implements Comparator<TaskInfo> {
+	
 	public int compare(TaskInfo task1, TaskInfo task2) {
-		if (task1.getStartDate() != null) {
-			return task2.getStartDate().compareTo(task1.getStartDate());
-		}
-		else {
-			return -1;
+		if (task1.getStartDate() != null && task2.getStartDate() != null) {
+			return task1.getStartDate().compareTo(task2.getStartDate());
+		} else if (task1.getEndDate() != null && task2.getEndDate() != null) {
+			return task1.getEndDate().compareTo(task2.getEndDate());
+		} else {
+			return 0;  //Both are equal
 		}
 	}
 }
