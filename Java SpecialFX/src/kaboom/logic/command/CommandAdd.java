4fefcecd80f8d40b72e953893b57d1191d2acfcd
Ivan Tjahjono <tjahjono.ivan@gmail.com>
2014-04-06 21:20:36 +0800

    changed error no name feedback for command add

--- Java SpecialFX/src/kaboom/logic/command/CommandAdd.java
+++ Java SpecialFX/src/kaboom/logic/command/CommandAdd.java
@@ -16,7 +16,7 @@ import kaboom.logic.TaskInfo;
 public class CommandAdd extends Command {
 	private final String MESSAGE_COMMAND_ADD_SUCCESS = "WOOT! <%1$s> ADDED. MORE STUFF TO DO!";
 	private final String MESSAGE_COMMAND_ADD_FAIL = "Fail to add <%1$s>... Error somewhere...";
-	private final String MESSAGE_COMMAND_ADD_FAIL_NO_NAME = "Enter a task name please :'(";
+	private final String MESSAGE_COMMAND_ADD_FAIL_NO_NAME = "Error! Task cannot be entered without a name Y_Y";
 	private final String MESSAGE_COMMAND_ADD_FAIL_STARTDATE_OVER_ENDDATE = "Wow! How did the task end before it even started? 0.0";
 	private final String MESSAGE_COMMAND_ADD_FAIL_INVALID_DATE = "Invalid date... Noob";
 
