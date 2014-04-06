08b793e515a3e5f2eaa1ab58cbda43f739c5ba6c
Abdul Hadi <damienhadios@gmail.com>
2014-04-06 20:34:33 +0800

    Increase maximum priority to 5, previously was 3.

--- Java SpecialFX/src/kaboom/logic/TextParser.java
+++ Java SpecialFX/src/kaboom/logic/TextParser.java
@@ -16,7 +16,7 @@ public class TextParser {
 	
 	private final String TIME_REGEX = "\\s+(([0-9]|0[0-9]|1[0-9]|2[0-3])([\\s?:\\s?]?[0-5][0-9])?|([0-9]|0[1-9]|1[0-2])(([\\s?:\\s?]?[0-5][0-9])?(am|pm)))(\\s|$)";
 	private final String DATE_REGEX = "\\s+\\d{1,2}[\\/\\.]?\\d{1,2}[\\/\\.]?\\d{2}(\\s|$)";
-	private final String PRIORITY_REGEX = "[\\s+]\\*{1,3}[\\s\\W]*";
+	private final String PRIORITY_REGEX = "[\\s+]\\*{1,5}[\\s\\W]*";
 	
 	static TextParser instance;
 	
