536ac455f4e1d86941615fbca0fa5f68f603f791
Abdul Hadi <damienhadios@gmail.com>
2014-04-08 19:00:13 +0800

    Text parser now allow date only for search.

---
+++ Java SpecialFX/src/kaboom/logic/command/CommandClear.java
@@ -0,0 +1,88 @@
+package kaboom.logic.command;
+
+import java.util.Hashtable;
+import java.util.Vector;
+
+import kaboom.logic.FormatIdentify;
+import kaboom.logic.KEYWORD_TYPE;
+import kaboom.logic.Result;
+import kaboom.logic.TaskInfo;
+
+
+public class CommandClear extends Command {
+	
+	private final String CLEAR_TYPE_ALL = "all";
+	private final String CLEAR_TYPE_CURRENT = "current";
+	private final String CLEAR_TYPE_EMPTY = "";
+	
+	private final String MESSAGE_COMMAND_CLEAR_SUCCESS = "1.. 2.. 3.. Pooof! Your schedule has gone with the wind";
+	private final String MESSAGE_COMMAND_CLEAR_FAIL_INVALID_TYPE = "1.. 2.. 3.. Pooof! Your schedule has gone with the wind";
+	private final String MESSAGE_COMMAND_CLEAR_FAIL_NO_TYPE = "please enter <clear all> to remove all tasks or <clear current> to remove current view";
+	private final String MESSAGE_COMMAND_CLEAR_FAIL_NOT_IMPLEMENTED = "LOL";
+	
+	Vector<TaskInfo> tasksCleared;
+	String clearType;
+		
+	public CommandClear () {
+		commandType = COMMAND_TYPE.CLEAR;
+		keywordList = new KEYWORD_TYPE[] {
+				KEYWORD_TYPE.CLEARTYPE
+		};
+		clearType = null;
+	}
+
+	public Result execute() {
+		assert taskListShop != null;
+		
+		clearType = infoTable.get(KEYWORD_TYPE.CLEARTYPE);
+		
+		if(clearType == null || clearType.isEmpty()) {
+			clearType = CLEAR_TYPE_ALL;
+		}
+		
+		String commandFeedback = "";
+		
+		switch (clearType) {
+		case CLEAR_TYPE_ALL:
+			tasksCleared = taskListShop.getAllCurrentTasks();		
+			commandFeedback = MESSAGE_COMMAND_CLEAR_SUCCESS;
+			addCommandToHistory ();
+			break;
+		case CLEAR_TYPE_CURRENT:
+			commandFeedback = MESSAGE_COMMAND_CLEAR_FAIL_NOT_IMPLEMENTED;
+			break;
+		case CLEAR_TYPE_EMPTY:
+			commandFeedback = MESSAGE_COMMAND_CLEAR_FAIL_NO_TYPE;
+			break;
+		default: 
+			commandFeedback = MESSAGE_COMMAND_CLEAR_FAIL_INVALID_TYPE;
+		}
+		
+		return createResult(commandFeedback);
+	}
+	
+	public boolean undo () {
+		boolean isUndoSuccessful = false;
+
+		for (int i = 0; i < tasksCleared.size(); i++) {
+			taskListShop.addTaskToList(tasksCleared.get(i));
+		}
+		
+		if (tasksCleared.size() == taskListShop.shopSize()) {
+			isUndoSuccessful = true;
+		}
+		
+		return isUndoSuccessful;
+	}
+	
+	public boolean parseInfo(String info, Vector<FormatIdentify> indexList) {
+		Hashtable<KEYWORD_TYPE, String> taskInformationTable = updateFormatList(info, indexList);
+		updateFormatListBasedOnHashtable(indexList, taskInformationTable);
+		
+		if (taskInformationTable.containsKey(KEYWORD_TYPE.INVALID)) {
+			return false;
+		}
+		
+		return true;
+	}
+}
---
+++ Java SpecialFX/src/kaboom/logic/command/CommandDone.java
@@ -0,0 +1,69 @@
+package kaboom.logic.command;
+
+import java.util.Hashtable;
+import java.util.Vector;
+
+import kaboom.logic.FormatIdentify;
+import kaboom.logic.KEYWORD_TYPE;
+import kaboom.logic.Result;
+import kaboom.logic.TaskInfo;
+import kaboom.ui.TaskView;
+
+public class CommandDone extends Command{
+	private final String MESSAGE_COMMAND_DONE_SUCCESS = "Set %1$s to complete";
+	private final String MESSAGE_COMMAND_DONE_AlEADY_COMPLETED = "%1$s was completed";
+
+	TaskInfo taskToBeModified;
+
+	public CommandDone() {
+		commandType = COMMAND_TYPE.DONE;
+		keywordList = new KEYWORD_TYPE[] {
+				KEYWORD_TYPE.TASKID,
+				KEYWORD_TYPE.TASKNAME
+		};
+	}
+
+	public Result execute() {
+		assert taskListShop != null;
+		
+		Result errorResult = taskDetectionWithErrorFeedback();
+		if(errorResult != null) {
+			return errorResult;
+		} else {
+			taskToBeModified = getTask();
+		}
+		
+		String feedback = MESSAGE_COMMAND_INVALID;
+		String taskName = taskToBeModified.getTaskName();
+		
+		if (taskToBeModified.getDone()) {
+			feedback = String.format(MESSAGE_COMMAND_DONE_AlEADY_COMPLETED, taskName);
+		} else {
+			taskListShop.setDoneByName(taskName);
+			taskView.deleteInView(taskToBeModified);
+			feedback = String.format(MESSAGE_COMMAND_DONE_SUCCESS, taskName);
+		}
+		
+		taskToBeModified.setRecent(true);
+		addCommandToHistory ();
+		return createResult(feedback);
+	}
+
+	public boolean undo() {
+		taskListShop.setLastToUndone();
+		taskView.addToView(taskToBeModified);
+		taskView.undoneInView(taskToBeModified);
+		return true;
+	}
+
+	public boolean parseInfo(String info, Vector<FormatIdentify> indexList) {
+		Hashtable<KEYWORD_TYPE, String> taskInformationTable = updateFormatList(info, indexList);
+		updateFormatListBasedOnHashtable(indexList, taskInformationTable);
+
+		if (taskInformationTable.containsKey(KEYWORD_TYPE.INVALID)) {
+			return false;
+		}
+
+		return true;
+	}
+}
