8a81a87c869061a2edaf6663454d86561df57a54
Abdul Hadi <damienhadios@gmail.com>
2014-04-09 23:08:55 +0800

    Add in class to focus in result class.

--- Java SpecialFX/src/kaboom/logic/Result.java
+++ Java SpecialFX/src/kaboom/logic/Result.java
@@ -1,19 +1,14 @@
 package kaboom.logic;
 
 import java.util.Vector;
-
 import kaboom.ui.DISPLAY_STATE;
 
-
-// TODO
-// 1) Have to include information to somehow inform GUI to go to next or previous or first page or last page
-//    of task display.
-// 2) If the taskToDisplayList is null, does it mean that it has nothing to show or remain to stick
-//    to what it is showing now ?
 
 public class Result {
 	Vector<TaskInfo> taskToDisplayList;
 	String			 feedback;
+	
+	TaskInfo		taskInFocus;
 	
 	boolean isGoToNextPage;
 	boolean isGoToPreviousPage;
@@ -22,6 +17,7 @@ public class Result {
 	
 	public Result () {
 		feedback = "";
+		taskInFocus = null;
 		isGoToNextPage = false;
 		isGoToPreviousPage = false;
 		stateToChangeTo = DISPLAY_STATE.INVALID;
@@ -31,32 +27,40 @@ public class Result {
 		taskToDisplayList = taskList;
 	}
 	
-	public Vector<TaskInfo> getTasksToDisplay () {
-		return taskToDisplayList;
-	}
-	
 	public void setFeedback (String newFeedback) {
 		feedback = newFeedback;
 	}
 	
-	public void setDisplayState (DISPLAY_STATE newState) {
-		stateToChangeTo = newState;
-	}
-	
-	public String getFeedback () {
-		return feedback;
+	public void setTaskToFocus (TaskInfo task) {
+		taskInFocus = task;
 	}
 	
 	public void setGoToNextPage (boolean flag) {
 		isGoToNextPage = flag;
 	}
 	
-	public boolean getGoToNextPage () {
-		return isGoToNextPage;
-	}
-	
 	public void setGoToPrevPage (boolean flag) {
 		isGoToPreviousPage = flag;
+	}
+	
+	public void setDisplayState (DISPLAY_STATE newState) {
+		stateToChangeTo = newState;
+	}
+	
+	public Vector<TaskInfo> getTasksToDisplay () {
+		return taskToDisplayList;
+	}
+	
+	public String getFeedback () {
+		return feedback;
+	}
+	
+	public TaskInfo getTaskToFocus () {
+		return taskInFocus;
+	}
+	
+	public boolean getGoToNextPage () {
+		return isGoToNextPage;
 	}
 	
 	public boolean getGoToPrevPage () {
