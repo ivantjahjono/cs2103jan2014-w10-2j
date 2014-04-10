c3e846684c41ef801b5829a3336b3c277b23ee9e
Tong Chun Kit <tongchunkit@gmail.com>
2014-04-10 14:55:03 +0800

    New comparator

--- Java SpecialFX/src/kaboom/logic/command/ComparatorDefault.java
+++ Java SpecialFX/src/kaboom/logic/command/ComparatorDefault.java
@@ -1,5 +1,6 @@
 package kaboom.logic.command;
 
+import java.util.Calendar;
 import java.util.Comparator;
 
 import kaboom.logic.TaskInfo;
@@ -7,15 +8,39 @@ import kaboom.logic.TaskInfo;
 public class ComparatorDefault implements Comparator<TaskInfo> {
 
 	public int compare(TaskInfo task1, TaskInfo task2) {
-		if (task1.getStartDate() != null && task2.getStartDate() != null) {
-			return task1.getStartDate().compareTo(task2.getStartDate());
+		final int COMPARE_EQUAL = 0;
+
+		int compareValue = compareDates(task1.getStartDate(), task2.getStartDate());
+		if (compareValue != COMPARE_EQUAL) {
+			return compareValue;
 		}
-		else if (task1.getEndDate() != null && task2.getEndDate() != null){
-			return task1.getEndDate().compareTo(task2.getEndDate());
+		
+		if (task1.getStartDate() != null && task2.getEndDate() != null) {
+			return 1;
 		}
-		else {
-			return task2.getPriority() - task1.getPriority();
+		
+		compareValue = compareDates(task1.getEndDate(), task2.getEndDate());
+		if (compareValue != COMPARE_EQUAL) {
+			return compareValue;
+		}
+		
+		compareValue = comparePriority(task1.getPriority(), task2.getPriority());
+		if (compareValue != COMPARE_EQUAL) {
+			return compareValue;
+		}
+		
+		return task1.getTaskName().compareToIgnoreCase(task2.getTaskName());
+	}
+
+	private int compareDates(Calendar date1, Calendar date2) {
+		if (date1 == null || date2 == null) {
+			return 0;
+		} else {
+			return date1.compareTo(date2);
 		}
 	}
 
+	private int comparePriority(int priority1, int priority2) {
+		return priority2 - priority1;
+	}
 }
