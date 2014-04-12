f105e171f95f9db0f61c4ab7d0936bab989af436
Tong Chun Kit <tongchunkit@gmail.com>
2014-04-13 01:17:55 +0800

    Added some comments

---
+++ Java SpecialFX/src/KaboomTest/StorageTest.java
@@ -0,0 +1,46 @@
+//@author A0096670W
+
+package KaboomTest;
+
+import static org.junit.Assert.*;
+
+import java.io.LineNumberReader;
+import java.io.FileReader;
+import java.io.File;
+import java.io.IOException;
+
+import kaboom.storage.Storage;
+import kaboom.storage.TaskDepository;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StorageTest {
+	
+	TaskDepository allTasks;
+	Storage storageTest;
+	
+	@Before
+	public void initailize() {
+		allTasks = TaskDepository.getInstance();
+		storageTest = new Storage("StorageTest.txt");
+		storageTest.load();
+	}
+	
+	@Test
+	public void testLoad() throws IOException {
+		LineNumberReader lineNumberReader = new LineNumberReader(new FileReader(new File("storageTest.txt")));
+		lineNumberReader.skip(Long.MAX_VALUE);  //Long.MAX_VALUE is more than 2 ExaBytes
+		assertEquals(allTasks.shopSize(), lineNumberReader.getLineNumber()*2);
+		lineNumberReader.close();
+	}
+
+	@Test
+	public void testStore() throws IOException {
+		storageTest.store();
+		LineNumberReader lineNumberReader = new LineNumberReader(new FileReader(new File("storageTest.txt")));
+		lineNumberReader.skip(Long.MAX_VALUE);  //Long.MAX_VALUE is more than 2 ExaBytes
+		assertEquals(allTasks.shopSize(), lineNumberReader.getLineNumber());
+		lineNumberReader.close();
+	}
+}
