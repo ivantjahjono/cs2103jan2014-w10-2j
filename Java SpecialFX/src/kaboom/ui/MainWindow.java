ec5f754561bc0fc26ad8a6a040acac30f217aac7
Abdul Hadi <damienhadios@gmail.com>
2014-04-13 00:56:38 +0800

    Remove unused functions.

--- Java SpecialFX/src/kaboom/ui/MainWindow.java
+++ Java SpecialFX/src/kaboom/ui/MainWindow.java
@@ -106,8 +106,6 @@ public class MainWindow implements javafx.fxml.Initializable, Observer {
 	@FXML private Pane 	helpSearchPane;
 		  private Pane 	activeHelpPanel;
 	
-	private Pane 	currentActiveHelpPane;
-	
 	// Tracks previous commands
 	private String 			currentCommand;
 	private Vector<String> 	commandsEnteredList;
@@ -394,69 +392,6 @@ public class MainWindow implements javafx.fxml.Initializable, Observer {
 		switch (command) {
 			case NEXT_PAGE_KEYWORD:
 			case PREV_PAGE_KEYWORD:
-				return true;
-				
-			default:
-				return false;
-		}
-	}
-	
-	private void activateHelpPane (String command) {
-		// Disable current help view
-		if (currentActiveHelpPane != null) {
-			currentActiveHelpPane.setVisible(false);
-		}
-		
-		Pane paneToActivate = getPaneBasedOnCommand(command);
-		if (currentActiveHelpPane != paneToActivate) {
-			currentActiveHelpPane = paneToActivate;
-			currentActiveHelpPane.setVisible(true);
-		} else {
-			currentActiveHelpPane = null;
-		}
-	}
-
-	private Pane getPaneBasedOnCommand(String command) {
-		switch (command) {
-			case "help":
-				return helpPane;
-				
-			case "help add":
-				return helpAddPane;
-				
-			case "help delete":
-				return helpDeletePane;
-				
-			case "help modify":
-				return helpModifyPane;
-				
-			case "help complete":
-				return helpCompletePane;
-				
-			case "help view":
-				return helpViewPane;
-				
-			case "help search":
-				return helpSearchPane;
-				
-			case "help close":
-				return currentActiveHelpPane;
-				
-			default:
-				return null;
-		}
-	}
-
-	private boolean isHelpCommand(String command) {
-		switch (command) {
-			case "help":
-			case "help add":
-			case "help delete":
-			case "help modify":
-			case "help complete":
-			case "help view":
-			case "help search":
-			case "help close":
 				return true;
 				
 			default:
@@ -755,6 +690,7 @@ public class MainWindow implements javafx.fxml.Initializable, Observer {
 	}
 	
 	private Rectangle createPageTabRectangle() {
+		@SuppressWarnings("rawtypes")
 		RectangleBuilder pageTabBuilder = RectangleBuilder.create()
 			    .x(100).y(100)
 			    .width(25).height(5)
