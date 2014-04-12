4a2980b855b9781c30cecf0650adcf250c640798
Tong Chun Kit <tongchunkit@gmail.com>
2014-04-12 22:54:35 +0800

    Modified priority comparator

--- Java SpecialFX/src/kaboom/shared/comparators/ComparatorPriority.java
+++ Java SpecialFX/src/kaboom/shared/comparators/ComparatorPriority.java
@@ -8,6 +8,21 @@ import kaboom.shared.TaskInfo;
 public class ComparatorPriority implements Comparator<TaskInfo> {
 
 	public int compare(TaskInfo task1, TaskInfo task2) {
-		return task2.getPriority() - task1.getPriority();
+		final int COMPARE_EQUAL = 0;
+		
+		int compareValue = comparePriority(task1.getPriority(), task2.getPriority());
+		if (compareValue != COMPARE_EQUAL) {
+			return compareValue;
+		} else {
+			return compareTaskName(task1.getTaskName(), task2.getTaskName());
+		}
+	}
+	
+	private int comparePriority(int task1Priority, int task2Priority) {
+		return task2Priority - task1Priority;
+	}
+	
+	private int compareTaskName(String name1, String name2) {
+		return name1.compareToIgnoreCase(name2);
 	}
 }
