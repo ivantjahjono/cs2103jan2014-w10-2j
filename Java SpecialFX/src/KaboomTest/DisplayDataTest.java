abbec4b9844b41eee7603b4c310c46810c4854bb
Abdul Hadi <damienhadios@gmail.com>
2014-04-13 13:14:51 +0800

    Update DisplayDataTest with more test cases.

--- Java SpecialFX/src/KaboomTest/DisplayDataTest.java
+++ Java SpecialFX/src/KaboomTest/DisplayDataTest.java
@@ -4,6 +4,7 @@ package KaboomTest;
 
 import static org.junit.Assert.*;
 import kaboom.shared.DISPLAY_STATE;
+import kaboom.shared.HELP_STATE;
 import kaboom.shared.Result;
 import kaboom.ui.DisplayData;
 
@@ -49,6 +50,47 @@ public class DisplayDataTest {
 		commandResult.setDisplayState(currentState);
 		displayData.updateDisplayWithResult(commandResult);
 		assertEquals(DISPLAY_STATE.EXPIRED, displayData.getCurrentDisplayState());
+	}
+	
+	@Test
+	public void testHelpState() {
+		Result commandResult = new Result();
+		
+		HELP_STATE currentState = HELP_STATE.INVALID;
+		commandResult.setHelpState(currentState);
+		displayData.updateDisplayWithResult(commandResult);
+		assertEquals(HELP_STATE.CLOSE, displayData.getCurrentHelpState());
+		
+		// Test if activate same state will give errors
+		currentState = HELP_STATE.INVALID;
+		commandResult.setHelpState(currentState);
+		displayData.updateDisplayWithResult(commandResult);
+		assertEquals(HELP_STATE.CLOSE, displayData.getCurrentHelpState());
+		
+		currentState = HELP_STATE.CLOSE;
+		commandResult.setHelpState(currentState);
+		displayData.updateDisplayWithResult(commandResult);
+		assertEquals(currentState, displayData.getCurrentHelpState());
+		
+		currentState = HELP_STATE.MAIN;
+		commandResult.setHelpState(currentState);
+		displayData.updateDisplayWithResult(commandResult);
+		assertEquals(currentState, displayData.getCurrentHelpState());
+		
+		currentState = HELP_STATE.MAIN;
+		commandResult.setHelpState(currentState);
+		displayData.updateDisplayWithResult(commandResult);
+		assertEquals(HELP_STATE.CLOSE, displayData.getCurrentHelpState());
+		
+		currentState = HELP_STATE.ADD;
+		commandResult.setHelpState(currentState);
+		displayData.updateDisplayWithResult(commandResult);
+		assertEquals(currentState, displayData.getCurrentHelpState());
+		
+		currentState = HELP_STATE.INVALID;
+		commandResult.setHelpState(currentState);
+		displayData.updateDisplayWithResult(commandResult);
+		assertEquals(HELP_STATE.ADD, displayData.getCurrentHelpState());
 	}
 
 }
