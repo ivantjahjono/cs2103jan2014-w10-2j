6a8d2ff9be5619cfa916773e9a1db98ec93d62d9
Wei <Wei@localhost>
2014-04-13 09:38:36 +0800

    Commented out unused functions in command

---
+++ Java SpecialFX/src/kaboom/logic/command/CommandSearch.java
@@ -0,0 +1,156 @@
+//@author A0096670W
+package kaboom.logic.command;
+
+import java.util.Calendar;
+import java.util.Hashtable;
+import java.util.Vector;
+
+import kaboom.shared.DISPLAY_STATE;
+import kaboom.shared.DateAndTimeFormat;
+import kaboom.shared.FormatIdentify;
+import kaboom.shared.KEYWORD_TYPE;
+import kaboom.shared.Result;
+import kaboom.shared.TaskInfo;
+
+public class CommandSearch extends Command {
+
+	private final String MESSAGE_COMMAND_SEARCH_SUCCESS = "Search done. %d item(s) found.";
+
+	TaskInfo taskInfo = null;
+	
+	public CommandSearch () {
+		commandType = COMMAND_TYPE.SEARCH;
+		keywordList = new KEYWORD_TYPE[] {
+				KEYWORD_TYPE.DATE,
+				KEYWORD_TYPE.END_TIME,
+				KEYWORD_TYPE.TASKNAME
+		};
+	}
+
+	public Result execute() {
+
+		assert taskListShop != null;
+		
+		//current extraction
+		DateAndTimeFormat dateAndTimeFormat = DateAndTimeFormat.getInstance();
+
+		String searchName = infoTable.get(KEYWORD_TYPE.TASKNAME).toLowerCase();
+		String searchOnDateInString = dateAndTimeFormat.convertStringDateToDayMonthYearFormat(infoTable.get(KEYWORD_TYPE.DATE));
+		String searchByDateInString = dateAndTimeFormat.convertStringDateToDayMonthYearFormat(infoTable.get(KEYWORD_TYPE.END_DATE));
+		
+		Calendar searchOnDate = dateAndTimeFormat.formatStringToCalendar(searchOnDateInString, dateAndTimeFormat.getEndTimeOfTheDay());
+		Calendar searchByDate = dateAndTimeFormat.formatStringToCalendar(searchByDateInString, dateAndTimeFormat.getStartTimeOfTheDay());
+		
+		//past extraction
+//		String searchName = taskInfo.getTaskName().toLowerCase();
+//		Calendar searchByDate = taskInfo.getEndDate();
+//		Calendar searchOnDate = taskInfo.getStartDate();
+
+		Vector<TaskInfo> tasksFound = new Vector<TaskInfo>();
+		Vector<TaskInfo> listToSearch;
+
+		if (searchByDate != null || searchOnDate != null) {
+			listToSearch = taskView.getAllPresentTasks();
+		} else {
+			listToSearch = taskView.getCurrentView();
+		}
+
+		if (!searchName.equals("")) {
+			for (int i = 0; i < listToSearch.size(); i++) {
+				TaskInfo singleTask = listToSearch.get(i);
+				if (singleTask.getTaskName().toLowerCase().contains(searchName)) {
+					tasksFound.add(singleTask);
+				}
+			}
+		} else if (searchOnDate != null){
+			//Search only on a particular day
+			for (int i = 0; i < listToSearch.size(); i++) {
+				TaskInfo singleTask = listToSearch.get(i);
+				if (isTaskOnDate(singleTask, searchOnDate)) {
+					tasksFound.add(singleTask);
+				}
+			}
+		} else if (searchByDate != null) {
+			//Cumulative search to a particular day
+			for (int i = 0; i < listToSearch.size(); i++) {
+				TaskInfo singleTask = listToSearch.get(i);
+				Calendar taskEndDate = singleTask.getEndDate();
+				if (taskEndDate != null) {
+					if (isTaskByDate(singleTask, searchByDate)) {
+						tasksFound.add(singleTask);
+					}
+				}
+			}
+		}
+
+		String commandFeedback = String.format(MESSAGE_COMMAND_SEARCH_SUCCESS, tasksFound.size());
+		taskView.setSearchView(tasksFound);
+
+		return createResult(commandFeedback, DISPLAY_STATE.SEARCH, null); 
+	}
+
+	public boolean parseInfo(String info, Vector<FormatIdentify> indexList) {
+		Hashtable<KEYWORD_TYPE, String> taskInformationTable = updateFormatList(info, indexList);
+		updateFormatListBasedOnHashtable(indexList, taskInformationTable);
+
+		if (taskInformationTable.containsKey(KEYWORD_TYPE.INVALID)) {
+			return false;
+		}
+
+		return true;
+	}
+	
+	//past extraction
+//	protected void storeTaskInfo(Hashtable<KEYWORD_TYPE, String> infoHashes) {	
+//		taskInfo = new TaskInfo();
+//		saveTaskPriority(infoHashes,taskInfo);
+//		saveTaskStartDateAndTime(infoHashes,taskInfo);
+//		saveTaskEndDateAndTime(infoHashes,taskInfo);
+//		saveTaskName(infoHashes,taskInfo);
+//	}
+
+	//Checks if tasks starts or ends on a particular date
+	//Checks the start time of timed tasks or in between
+	//Checks the end time of deadline tasks
+	private boolean isTaskOnDate(TaskInfo task, Calendar searchOnDate) {
+		Calendar taskStartDate = task.getStartDate();
+		Calendar taskEndDate = task.getEndDate();
+
+		if (taskStartDate != null) {
+			if (taskStartDate.get(Calendar.DAY_OF_YEAR) == searchOnDate.get(Calendar.DAY_OF_YEAR) &&
+					taskStartDate.get(Calendar.YEAR) == searchOnDate.get(Calendar.YEAR)) {
+				return true;
+			} else if (taskStartDate.before(searchOnDate) && taskEndDate.after(searchOnDate)) {
+				//For tasks that start before and ends after the search date
+				//Since there is a start date, there must surely be an end date
+				return true;
+			}
+		} else if (taskEndDate != null) {
+			if (taskEndDate.get(Calendar.DAY_OF_YEAR) == searchOnDate.get(Calendar.DAY_OF_YEAR) &&
+					taskEndDate.get(Calendar.YEAR) == searchOnDate.get(Calendar.YEAR)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	//Searches tasks that ends by that particular date
+	//Does not include tasks that start before but end after that date
+	private boolean isTaskByDate(TaskInfo task, Calendar searchByDate) {
+		Calendar taskEndDate = task.getEndDate();
+		Calendar today = Calendar.getInstance();
+		today.set(Calendar.HOUR_OF_DAY, 00);
+		today.set(Calendar.MINUTE, 00);
+		today.set(Calendar.SECOND, 00);
+
+		if (taskEndDate != null) {
+			if (taskEndDate.get(Calendar.DAY_OF_YEAR) == searchByDate.get(Calendar.DAY_OF_YEAR) &&
+					taskEndDate.get(Calendar.YEAR) == searchByDate.get(Calendar.YEAR)) {
+				return true;
+			} else if (taskEndDate.after(today) && taskEndDate.before(searchByDate)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ Java SpecialFX/src/kaboom/ui/GraphicInterface.java
@@ -0,0 +1,105 @@
+//@author A0099175N
+
+package kaboom.ui;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.util.Duration;
+import kaboom.logic.TaskMasterKaboom;
+
+
+public class GraphicInterface extends Application {
+	private final int UPDATE_INTERVAL = 30;
+	private final int WINDOW_WIDTH  = 700;
+	private final int WINDOW_HEIGHT = 700;
+	
+	private final String APPLICATION_NAME = "Task Master Kaboom";
+	private final String ICON_FILENAME = "img/taskmasterkaboom.png";
+	private final String FXML_FILENAME = "TaskMasterKaboomUiUpgrade.fxml";
+	
+	Parent root;
+	MainWindow mainWindow;
+	
+	UpdateService myService;
+	Timeline updateTimeline;
+	
+	private static TaskMasterKaboom controllerInstance;
+	static int counter = 0;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		FXMLLoader loader = null;
+		try {
+			loader = new FXMLLoader(getClass().getResource(FXML_FILENAME));
+			root = (Parent)(loader.load());
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		mainWindow = loader.getController();
+		mainWindow.setStage(primaryStage);
+		
+		Scene scene = new Scene(root);
+		scene.setFill(null);
+		setupStage(primaryStage, scene);
+		
+		mainWindow.prepareTextfieldFocus();
+
+		updateApplicationIconAndTitle(primaryStage);
+		
+		updateTimeline = setupRunningUpdate();
+		initialiseAndStartUpdateService();
+	}
+
+	private void setupStage(Stage primaryStage, Scene scene) {
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+		primaryStage.initStyle(StageStyle.TRANSPARENT);
+		primaryStage.setWidth(WINDOW_WIDTH);
+		primaryStage.setHeight(WINDOW_HEIGHT);
+		primaryStage.show();
+	}
+
+	private void updateApplicationIconAndTitle(Stage primaryStage) {
+		Image ico = new Image(this.getClass().getResourceAsStream(ICON_FILENAME));
+		primaryStage.getIcons().add(ico);
+		primaryStage.setTitle(APPLICATION_NAME);
+	}
+
+	private void initialiseAndStartUpdateService() {
+		updateTimeline.setCycleCount(Animation.INDEFINITE);
+		updateTimeline.playFrom("end");
+	}
+
+	private Timeline setupRunningUpdate() {
+		myService = new UpdateService(mainWindow);
+
+	    Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(UPDATE_INTERVAL), new EventHandler<ActionEvent>() {
+	    	@Override
+			public void handle(ActionEvent event) {
+	    		controllerInstance.updateTaskList();
+	    	}
+	    }));
+		return timeline;
+	}
+	
+	public static void run(String[] args) {
+		launch(args);
+	}
+	
+	public static void main (String[] args) {
+		controllerInstance = TaskMasterKaboom.getInstance();
+		controllerInstance.initialiseKaboom(); 
+		run(args);
+	}
+}
