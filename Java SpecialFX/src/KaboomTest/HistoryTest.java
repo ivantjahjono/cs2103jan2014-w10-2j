111617eb78b3aedfc99b99b2f0a5b5991bb436d2
Abdul Hadi <damienhadios@gmail.com>
2014-04-13 11:39:57 +0800

    Add in help page for page.

---
+++ Java SpecialFX/src/KaboomTest/HistoryTest.java
@@ -0,0 +1,63 @@
+//@author A0096670W
+
+package KaboomTest;
+
+import static org.junit.Assert.*;
+
+import kaboom.logic.command.Command;
+import kaboom.logic.command.CommandAdd;
+import kaboom.logic.command.CommandClear;
+import kaboom.logic.command.CommandView;
+import kaboom.storage.History;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HistoryTest {
+	
+	History history;
+	Command commandAdd;
+	Command commandClear;
+	Command commandView;
+	
+	@Before
+	public void initialize() {
+		history = History.getInstance();
+		commandAdd = new CommandAdd();
+		commandClear = new CommandClear();
+		commandView = new CommandView();
+	}
+	
+	@Test
+	public void testGetInstance() {
+		assertNotNull(history);
+	}
+	
+	@Test
+	public void testAddToRecentCommands() {
+		history.clear();
+		history.addToRecentCommands(commandClear);
+		assertEquals(1, history.size());
+		history.addToRecentCommands(commandAdd);
+		assertEquals(2, history.size());
+	}
+
+	@Test
+	public void testGetMostRecentCommand() {
+		history.clear();
+		history.addToRecentCommands(commandAdd);
+		history.addToRecentCommands(commandClear);
+		assertEquals(history.getMostRecentCommand(), commandClear);
+		assertEquals(1, history.size());
+		assertEquals(history.getMostRecentCommand(), commandAdd);
+		assertEquals(0, history.size());
+	}
+
+	@Test
+	public void testGetMostRecentCommandView() {
+		history.addToRecentCommands(commandAdd);
+		assertNotEquals(history.getMostRecentCommandView(), commandAdd);
+		history.addToRecentCommands(commandView);
+		assertEquals(history.getMostRecentCommand(), commandView);
+	}
+}
---
+++ Java SpecialFX/src/kaboom/logic/command/CommandFactory.java
@@ -0,0 +1,132 @@
+//@author A0073731J
+
+package kaboom.logic.command;
+
+import java.util.Hashtable;
+import java.util.Vector;
+
+import kaboom.logic.TextParser;
+import kaboom.shared.KEYWORD_TYPE;
+
+public class CommandFactory {
+	private static final String KEYWORD_COMMAND_ADD 	= "add";
+	private static final String KEYWORD_COMMAND_DELETE 	= "delete";
+	private static final String KEYWORD_COMMAND_MODIFY 	= "modify";
+	private static final String KEYWORD_COMMAND_SEARCH 	= "search";
+	private static final String KEYWORD_COMMAND_CLEAR 	= "clear";
+	private static final String KEYWORD_COMMAND_VIEW 	= "view";
+	private static final String KEYWORD_COMMAND_UNDO 	= "undo";
+	private static final String KEYWORD_COMMAND_DONE 	= "boom";
+	private static final String KEYWORD_COMMAND_UNDONE 	= "unboom";
+	private static final String KEYWORD_COMMAND_HELP 	= "help";
+	private static final String KEYWORD_COMMAND_PAGE 	= "page";
+	
+	private static TextParser textParser = TextParser.getInstance();
+	
+	public static Command createCommand(String userInputSentence) {
+		
+		//1. Get Command 
+		String commandKeyword = textParser.getCommandKeyWord(userInputSentence);
+		
+		//2. Get Command keyword
+		COMMAND_TYPE commandType = determineCommandType(commandKeyword);
+		
+		//3. Create Command
+		Command commandToExecute = createCommandBasedOnCommandType(commandType);	
+		
+		//4. Remove Command Word From UserInput
+		userInputSentence = textParser.removeFirstWord(userInputSentence);
+		
+		//5.Initialise variables
+		commandToExecute.initialiseCommandInfoTable(userInputSentence);
+		
+		return commandToExecute;
+	}
+	
+	private static COMMAND_TYPE determineCommandType(String commandWord) {
+		commandWord = commandWord.toLowerCase();
+		switch(commandWord) {
+			case KEYWORD_COMMAND_ADD:
+				return COMMAND_TYPE.ADD;
+			case KEYWORD_COMMAND_DELETE:
+				return COMMAND_TYPE.DELETE;
+			case KEYWORD_COMMAND_MODIFY:
+				return COMMAND_TYPE.MODIFY;
+			case KEYWORD_COMMAND_SEARCH:
+				return COMMAND_TYPE.SEARCH;
+			case KEYWORD_COMMAND_CLEAR:
+				return COMMAND_TYPE.CLEAR;
+			case KEYWORD_COMMAND_VIEW:
+				return COMMAND_TYPE.VIEW;
+			case KEYWORD_COMMAND_UNDO:
+				return COMMAND_TYPE.UNDO;
+			case KEYWORD_COMMAND_DONE:
+				return COMMAND_TYPE.DONE;
+			case KEYWORD_COMMAND_UNDONE:
+				return COMMAND_TYPE.UNDONE;
+			case KEYWORD_COMMAND_HELP:
+				return COMMAND_TYPE.HELP;
+			case KEYWORD_COMMAND_PAGE:
+				return COMMAND_TYPE.PAGE;
+			default:
+				return COMMAND_TYPE.INVALID;
+		}
+	}
+	
+	private static Command createCommandBasedOnCommandType (COMMAND_TYPE commandType) {
+		Command newlyCreatedCommand = new Command();
+		
+		switch (commandType) {
+			case ADD:
+				newlyCreatedCommand = new CommandAdd();
+				break;
+				
+			case DELETE:
+				newlyCreatedCommand = new CommandDelete();
+				break;
+				
+			case MODIFY:
+				newlyCreatedCommand = new CommandModify();
+				break;
+				
+			case SEARCH:
+				newlyCreatedCommand = new CommandSearch();
+				break;
+				
+			case CLEAR:
+				newlyCreatedCommand = new CommandClear();
+				break;
+				
+			case VIEW:
+				newlyCreatedCommand = new CommandView();
+				break;
+				
+			case UNDO:
+				newlyCreatedCommand = new CommandUndo();
+				break;
+				
+			case DONE:
+				newlyCreatedCommand = new CommandDone();
+				break;
+				
+			case UNDONE:
+				newlyCreatedCommand = new CommandUndone();
+				break;
+				
+			case HELP:
+				newlyCreatedCommand = new CommandHelp();
+				break;
+				
+			case PAGE:
+				newlyCreatedCommand = new CommandPage();
+				break;
+				
+			default:
+				newlyCreatedCommand = new Command();
+				break;
+				
+		}
+		
+		return newlyCreatedCommand;
+	}
+}
