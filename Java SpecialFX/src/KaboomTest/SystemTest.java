144e08c46595d055e136b85d2f9010467583eaa4
Wei <Wei@localhost>
2014-04-14 18:37:25 +0800

    Merge

--- Java SpecialFX/src/KaboomTest/SystemTest.java
+++ Java SpecialFX/src/KaboomTest/SystemTest.java
@@ -145,14 +145,162 @@ public class SystemTest {
 		command = "modify 1 by today";
 		assertEquals("<hello> has manipulated time", controller.processCommand(command));
 		
+		command = "modify 1 by 1";
+		assertEquals("<hello> has manipulated time", controller.processCommand(command));
+		
+		command = "modify 1 BY 1";
+		assertEquals("Oops! Invalid taskname??", controller.processCommand(command));
+		
 		command = "modify hello by 2pm";
 		assertEquals("<hello> has manipulated time", controller.processCommand(command));
 		
 		command = "modify hello at 2pm";
 		assertEquals("<hello> has manipulated time", controller.processCommand(command));
 		
+		command = "modify hello at 2   pm";
+		assertEquals("Oops! No such task exist", controller.processCommand(command));
+		
 		command = "modify 1 at 2pm";
 		assertEquals("<hello> has manipulated time", controller.processCommand(command));
+	}
+	
+	@Test
+	public void testSearchByName () {
+		controller.processCommand("clear all");
+		String command = "";
+		
+		command = "add hello";
+		assertEquals("WOOT! <hello> ADDED. MORE STUFF TO DO!", controller.processCommand(command));
+		
+		command = "search";
+		assertEquals("Please enter something to search", controller.processCommand(command));
+		
+		command = "        search";
+		assertEquals("Please enter something to search", controller.processCommand(command));
+		
+		command = "search h";
+		assertEquals("Search done. 1 item(s) found.", controller.processCommand(command));
+		
+		command = "search helloo";
+		assertEquals("Search done. 0 item(s) found.", controller.processCommand(command));
+		
+		command = "search h ello";
+		assertEquals("Search done. 0 item(s) found.", controller.processCommand(command));
+		
+		command = "search            * hello";
+		assertEquals("Search done. 0 item(s) found.", controller.processCommand(command));
+		
+		command = "search llo";
+		assertEquals("Search done. 1 item(s) found.", controller.processCommand(command));
+		
+		command = "search        ";
+		assertEquals("Please enter something to search", controller.processCommand(command));
+		
+		command = "search **";
+		assertEquals("Search done. 0 item(s) found.", controller.processCommand(command));
+	}
+	
+	@Test
+	public void testSearchByDate () {
+		controller.processCommand("clear all");
+		String command = "";
+		
+		command = "add hello by tmr";
+		assertEquals("WOOT! <hello> ADDED. MORE STUFF TO DO!", controller.processCommand(command));
+
+		command = "search by tmr";
+		assertEquals("Search done. 1 item(s) found.", controller.processCommand(command));
+		
+		command = "search by today";
+		assertEquals("Search done. 0 item(s) found.", controller.processCommand(command));
+		
+		command = "search by today by today";
+		assertEquals("Search done. 0 item(s) found.", controller.processCommand(command));
+		
+		command = "search by tomorrow";
+		assertEquals("Search done. 1 item(s) found.", controller.processCommand(command));
+		
+		command = "search to tomorrow";
+		assertEquals("Search done. 1 item(s) found.", controller.processCommand(command));
+		
+		command = "search to tomorrow ";
+		assertEquals("Search done. 1 item(s) found.", controller.processCommand(command));
+		
+		// The search should not accept something as time
+		command = "search by something";
+		assertEquals("Search done. 0 item(s) found.", controller.processCommand(command));
+	}
+	
+	@Test
+	public void testClear () {
+		controller.processCommand("clear all");
+		String command = "";
+		
+		command = "clear";
+		assertEquals("Enter <clear all> to remove all tasks or <clear present> to remove current view", controller.processCommand(command));
+		
+		command = "Clear";
+		assertEquals("Enter <clear all> to remove all tasks or <clear present> to remove current view", controller.processCommand(command));
+		
+		command = "clear    ";
+		assertEquals("Enter <clear all> to remove all tasks or <clear present> to remove current view", controller.processCommand(command));
+		
+		command = "clear all";
+		assertEquals("1.. 2.. 3.. Pooof! Your schedule has gone with the wind", controller.processCommand(command));
+		
+		command = "clear All";
+		assertEquals("Enter <clear all> to remove all tasks or <clear present> to remove current view", controller.processCommand(command));
+		
+		command = "clear all     ";
+		assertEquals("1.. 2.. 3.. Pooof! Your schedule has gone with the wind", controller.processCommand(command));
+		
+		command = "clear present";
+		assertEquals("1.. 2.. 3.. Pooof! Your present schedule has gone with the wind", controller.processCommand(command));
+		
+		command = "clear archive";
+		assertEquals("3.. 2.. 1.. Pooof! Your archive has gone with the wind", controller.processCommand(command));
+	}
+	
+	@Test
+	public void testCompleteIncomplete () {
+		controller.processCommand("clear all");
+		String command = "";
+		
+		command = "add hello";
+		assertEquals("WOOT! <hello> ADDED. MORE STUFF TO DO!", controller.processCommand(command));
+		
+		command = "boom hello";
+		assertEquals("Set hello to complete", controller.processCommand(command));
+		
+		command = "boom hello";
+		assertEquals("Oops! No such task exist", controller.processCommand(command));
+		
+		command = "unboom hello";
+		assertEquals("Oops! No such task exist", controller.processCommand(command));
+		
+		command = "view archive";
+		controller.processCommand(command);
+		
+		command = "unboom hello";
+		assertEquals("Set hello to incomplete", controller.processCommand(command));
+		
+		command = "unboom hello";
+		assertEquals("Oops! No such task exist", controller.processCommand(command));
+		
+		command = "add hello";
+		assertEquals("WOOT! <hello> ADDED. MORE STUFF TO DO!", controller.processCommand(command));
+		
+		command = "boom hello";
+		assertEquals("Search done. 2 item(s) found.", controller.processCommand(command));
+		
+		command = "unboom hello";
+		assertEquals("Search done. 2 item(s) found.", controller.processCommand(command));
+		
+		command = "delete 1";
+		controller.processCommand(command);
+		
+		command = "unboom hello";
+		assertEquals("hello was incomplete", controller.processCommand(command));
 	}
 
 	@After
