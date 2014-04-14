730848a6b6326605902b170e54e5f9ba73cdb339
Ivan Tjahjono <tjahjono.ivan@gmail.com>
2014-04-14 15:04:48 +0800

    changed message to no such task exist

---
+++ Java SpecialFX/src/kaboom/logic/command/CommandDone.java
@@ -0,0 +1,80 @@
+//@author A0096670W
+
+package kaboom.logic.command;
+
+import java.util.Hashtable;
+import java.util.Vector;
+
+import kaboom.shared.FormatIdentify;
+import kaboom.shared.KEYWORD_TYPE;
+import kaboom.shared.Result;
+import kaboom.shared.TaskInfo;
+
+public class CommandDone extends Command {
+	private final String MESSAGE_COMMAND_DONE_SUCCESS = "Set %1$s to complete";
+	private final String MESSAGE_COMMAND_DONE_AlEADY_COMPLETED = "%1$s was already completed";
+	private final String MESSAGE_COMMAND_INVALID = "Nope. No such BOOM command. Type <help complete> for help.";
+	
+	TaskInfo taskToBeModified;
+
+	public CommandDone() {
+		commandType = COMMAND_TYPE.DONE;
+		keywordList = new KEYWORD_TYPE[] {
+				KEYWORD_TYPE.TASKID,
+				KEYWORD_TYPE.TASKNAME
+		};
+	}
+
+	public Result execute() {
+		assert taskView != null;
+		
+//		Result errorResult = invalidTaskNameAndClashErrorDetection();
+//		if(errorResult != null) {
+//			return errorResult;
+//		} else {
+//			taskToBeModified = getTask();
+//		}
+		
+		if (infoTable.containsKey(KEYWORD_TYPE.INVALID)) {
+			return createResult(MESSAGE_COMMAND_INVALID);
+		}
+		
+		COMMAND_ERROR commandError = errorDetectionForInvalidTaskNameAndId();
+		if(commandError != null) {
+			return commandErrorHandler(commandError);
+		} else {
+			taskToBeModified = getTask();
+		}
+		
+		String feedback = MESSAGE_COMMAND_INVALID;
+		String taskName = taskToBeModified.getTaskName();
+		
+		if (taskToBeModified.getDone()) {
+			feedback = String.format(MESSAGE_COMMAND_DONE_AlEADY_COMPLETED, taskName);
+		} else {
+			taskView.doneTask(taskToBeModified);
+			addCommandToHistory();
+			feedback = String.format(MESSAGE_COMMAND_DONE_SUCCESS, taskName);
+		}
+		
+		return createResult(feedback);
+	}
+
+	public boolean undo() {
+		taskToBeModified.setDone(false);
+		taskView.refreshTasksFlagsOnly();  //Refresh to shift task to current
+		taskView.addToSearchView(taskToBeModified);
+		return true;
+	}
+
+	public boolean parseInfo(String info, Vector<FormatIdentify> indexList) {
+		Hashtable<KEYWORD_TYPE, String> taskInformationTable = updateFormatList(info, indexList);
+		updateFormatListBasedOnHashtable(indexList, taskInformationTable);
+
+		if (taskInformationTable.containsKey(KEYWORD_TYPE.INVALID)) {
+			return false;
+		}
+
+		return true;
+	}
+}
